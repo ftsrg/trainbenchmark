c31ecc9dd97162aa2fd2dd7c406efe7d110df791
Gabor Szarnyas <szarnyasg@gmail.com>
2015-01-25 17:42:49 +0100

    Added Travis conf

---
+++ scripts/resolve-dependencies.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd "$( cd "$( dirname "$0" )" && pwd )/../.."
+
+if [ ! -d neo4j-shell-tools ]; then
+  git clone https://github.com/szarnyasg/neo4j-shell-tools
+fi
+cd neo4j-shell-tools
+mvn clean install -DskipTests
+cd ..
+
+if [ ! -d rdf-graph-drivers ]; then
+  git clone https://github.com/FTSRG/rdf-graph-drivers
+fi
+cd rdf-graph-drivers
+scripts/resolve-dependencies
+scripts/build.sh
+mvn clean install -P fourstore -DskipTests
+cd ..
