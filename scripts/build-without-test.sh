5fc38c3134749a38bc59d041e1f858c76442cd7c
ZsoltKovari <kovari.zsolt91@gmail.com>
2015-02-04 17:40:26 +0100

    Add mvn install-file to shell scripts

--- scripts/build-without-test.sh
+++ scripts/build-without-test.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 cd "$( cd "$( dirname "$0" )" && pwd )/.."
-
+mvn install:install-file -Dfile=./hu.bme.mit.trainbenchmark.benchmark.virtuoso/src/main/resources/virt_sesame2.jar -DgroupId=virtuoso \
+                     -DartifactId=virtuoso-sesame2 -Dversion=2.7.3 \
+                     -Dpackaging=jar
+mvn install:install-file -Dfile=./hu.bme.mit.trainbenchmark.benchmark.virtuoso/src/main/resources/virtjdbc4.jar -DgroupId=com.virtuoso.virtjdbc4 \
+                     -DartifactId=virtjdbc4 -Dversion=3.0 \
+                     -Dpackaging=jar
 export MAVEN_OPTS="-XX:MaxPermSize=128M"
 mvn clean install -DskipTests
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
+mvn install:install-file -Dfile=./hu.bme.mit.trainbenchmark.benchmark.virtuoso/src/main/resources/virt_sesame2.jar -DgroupId=virtuoso \
+                     -DartifactId=virtuoso-sesame2 -Dversion=2.7.3 \
+                     -Dpackaging=jar
+mvn install:install-file -Dfile=./hu.bme.mit.trainbenchmark.benchmark.virtuoso/src/main/resources/virtjdbc4.jar -DgroupId=com.virtuoso.virtjdbc4 \
+                     -DartifactId=virtjdbc4 -Dversion=3.0 \
+                     -Dpackaging=jar
 mvn clean install --fail-at-end
