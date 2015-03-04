1dac5ee07b5b5d9de0d8d6478c57d7bd239e0c46
ZsoltKovari <kovari.zsolt91@gmail.com>
2015-03-04 15:07:50 +0100

    Turn off jackson autodetection, plus some changes in Results

--- hu.bme.mit.trainbenchmark.benchmark/src/main/java/hu/bme/mit/trainbenchmark/benchmark/scenarios/UserScenario.java
+++ hu.bme.mit.trainbenchmark.benchmark/src/main/java/hu/bme/mit/trainbenchmark/benchmark/scenarios/UserScenario.java
@@ -33,8 +33,8 @@ public class UserScenario implements Scenario<AbstractBenchmarkCase<?>> {
 		testCase.benchmarkDestroy();
 
 		final BenchmarkResult bmr = testCase.getBenchmarkResult();
-		// JsonSerializer.serialize(bmr);
 		System.out.println(bmr);
+		bmr.publish();
 		return bmr;
 	}
 }
