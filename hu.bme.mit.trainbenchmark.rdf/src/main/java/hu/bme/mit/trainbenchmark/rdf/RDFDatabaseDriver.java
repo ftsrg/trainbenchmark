86804cbe131e8352ce90c7580fb63bb1adfddfcb
Gabor Szarnyas <szarnyasg@gmail.com>
2015-03-05 14:42:35 +0100

    Added RDF database driver

---
+++ hu.bme.mit.trainbenchmark.rdf/src/main/java/hu/bme/mit/trainbenchmark/rdf/RDFDatabaseDriver.java
@@ -0,0 +1,33 @@
+package hu.bme.mit.trainbenchmark.rdf;
+
+import static hu.bme.mit.trainbenchmark.rdf.RDFConstants.BASE_PREFIX;
+import static hu.bme.mit.trainbenchmark.rdf.RDFConstants.ID_PREFIX;
+import static hu.bme.mit.trainbenchmark.rdf.RDFConstants.RDF_TYPE;
+import hu.bme.mit.trainbenchmark.benchmark.driver.DatabaseDriver;
+
+import java.io.IOException;
+
+public abstract class RDFDatabaseDriver<T> extends DatabaseDriver<T> {
+	
+	protected Long determineNewVertexId() throws IOException {
+		Long id = 5000L;
+		// safety measure to avoid infinite loop in case of a driver bug
+		int iterationCount = 1;
+
+		final String askQuery = "PREFIX base: <" + BASE_PREFIX + "> " //
+				+ "PREFIX rdf:  <" + RDF_TYPE + "> " //
+				+ "ASK { base:" + ID_PREFIX + "%d ?y ?z }";
+		while (iterationCount <= 20 && ask(String.format(askQuery, id))) {
+			id *= 2;
+			iterationCount++;
+		}
+		if (iterationCount > 20) {
+			throw new IOException("Could not generate new unique id.");
+		}
+
+		return id;
+	}
+	
+	protected abstract boolean ask(String askQuery) throws IOException;
+	
+}
