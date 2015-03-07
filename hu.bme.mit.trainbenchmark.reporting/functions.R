0b76d67bd47236ecd098760cb7ba4939a8a21886
ZsoltKovari <kovari.zsolt91@gmail.com>
2015-03-07 15:06:52 +0100

    New json structure and new util functions in reporting

--- hu.bme.mit.trainbenchmark.reporting/functions.R
+++ hu.bme.mit.trainbenchmark.reporting/functions.R
@@ -1,22 +1,16 @@
 savePlot <-function(results, settings, func, fileName){
-  if(length(func) > 1){
-    first <- TRUE
-    for(phase in func){
-      if (first == TRUE){
-        merged <- subset(results, PhaseName == phase) # selection on phase
-        first <- FALSE
-      }
-      else{
-        merged <- rbind(merged, subset(results, PhaseName == phase)) # merge back
-      }
+  first <- TRUE
+  for(phase in func){
+    if (first == TRUE){
+      merged <- subset(results, PhaseName == phase) # selection on phase
+      first <- FALSE
     }
-  }
-  else{
-    merged <- subset(results, PhaseName == func)
+    else{
+      merged <- rbind(merged, subset(results, PhaseName == phase)) # merge back
+    }
   }
   
   if (nrow(merged) == 0) {
-    print("Empty merged frame")
     return()
   }
   if (settings@xDimension == "Size"){
@@ -37,9 +31,6 @@ savePlot <-function(results, settings, func, fileName){
   plot <- ggplot(data,aes_string(x = settings@xDimension, y = settings@yDimension)) +
     geom_line(aes_string(group = settings@group, colour=settings@group)) + 
     geom_point(aes_string(shape = settings@group, colour=settings@group)) + 
-    scale_y_log10(breaks = round(10^seq(log10(minValue), log10(maxValue), by=((log10(maxValue)-log10(minValue))/7)),7), 
-                  labels = round(10^seq(log10(minValue), log10(maxValue), by=((log10(maxValue)-log10(minValue))/7)),2)
-    ) + 
     ylab(settings@yLabel) +
     xlab(settings@xLabel) +
     ggtitle(label = settings@title)
@@ -50,6 +41,16 @@ savePlot <-function(results, settings, func, fileName){
   else if (settings@xAxis == "Log10"){
     plot <- plot + scale_x_log10(breaks = c(artifacts), labels = c(artifacts))
   }
+
+  if (settings@yAxis == "Continuous"){
+    plot <- plot + scale_y_continuous(breaks = seq(minValue, maxValue, by=(maxValue-minValue)/7),
+                                      labels = round(seq(minValue, maxValue, by=(maxValue-minValue)/7),2))
+  }
+  else if (settings@yAxis == "Log10"){
+    plot <- plot + scale_y_log10(breaks = round(10^seq(log10(minValue), log10(maxValue), by=((log10(maxValue)-log10(minValue))/7)),7), 
+                                labels = round(10^seq(log10(minValue), log10(maxValue), by=((log10(maxValue)-log10(minValue))/7)),2))
+  }
+
   ggsave(plot,filename = fileName, width=14, height=7, dpi=192)
   print(fileName)
 }
@@ -57,19 +58,37 @@ savePlot <-function(results, settings, func, fileName){
 validConfig <- function(results, functions){
   uniquePhases <- unique(results$PhaseName)
   for(func in functions){
-    if(length(func) > 1){
-      for(phase in func){
-        if(phase %in% uniquePhases == FALSE){
-          return(FALSE)
-        }
-      }
-    }
-    else{
-      if(func %in% uniquePhases == FALSE){
+    for(phase in func){
+      if(phase %in% uniquePhases == FALSE){
         return(FALSE)
       }
     }
   }
   return(TRUE)
-  
+}
+
+createFolders <- function(rootPath, subFolders){
+  if (file.exists(rootPath) == FALSE){
+    dir.create(rootPath)
+  }
+  for (folder in subFolders){
+    path <- paste(rootPath, folder, sep='')
+    if (file.exists(path) == FALSE){
+      dir.create(path)
+    }
+  }
+}
+
+concatPhases <- function(phases){
+  first <- TRUE
+  for(p in phases){
+    if(first){
+      merged <- p
+      first <- FALSE
+    }
+    else{
+      merged <- paste(merged, p, sep='+')
+    }
+  }
+  return(merged)
 }
