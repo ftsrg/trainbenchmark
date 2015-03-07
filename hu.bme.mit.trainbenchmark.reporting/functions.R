9d75cac109966d69cafe785c8f45a0dfc8fb3694
ZsoltKovari <kovari.zsolt91@gmail.com>
2015-03-07 01:31:00 +0100

    Rewrite reporting scripts, initial version

--- hu.bme.mit.trainbenchmark.reporting/functions.R
+++ hu.bme.mit.trainbenchmark.reporting/functions.R
@@ -1,22 +1,41 @@
-# load .json files and convert into nested lists
-load_results <-function(){
-  files <- dir("../results/", pattern = '\\.json', full.names = TRUE)
-  lists <- lapply(files,fromJSON)
-}
-
-
-change_time_metrics <- function(table, divisor){
-  table$readTime <- (table$readTime / divisor)
-  table$checkTimeSum <- (table$checkTimeSum / divisor)
-  table$editTimeSum <- (table$editTimeSum / divisor)
-  table$checkTimes <- (table$checkTimes / divisor)
-  table$editTimes <- (table$editTimes / divisor)
-  return(table)
-}
-
-
-get_artifacts <- function(table){
-  artifacts <- unique(table$artifactSize)
-  artifacts <- sort(artifacts)
-  return(artifacts)
+savePlot <-function(results, settings, func, fileName){
+  results$MetricValue <- results$MetricValue / 10**6
+  
+  first <- TRUE
+  for(phase in func){
+    if (first == TRUE){
+      merged <- subset(results, PhaseName == phase) # selection on phase
+      first <- FALSE
+    }
+    else{
+      merged <- rbind(merged, subset(results, PhaseName == phase)) # merge back
+    }
+  }
+  
+  if (nrow(merged) == 0) 
+    return()
+  if (settings@xDimension == "Size"){
+    # summarise the iterations
+    data <- ddply(merged, c("Size", "CaseName", "Tool", "Scenario", "RunIndex", "MetricName"),
+                  summarise, MetricValue=sum(MetricValue))
+    data <- ddply(data, c("Size", "CaseName", "Tool", "Scenario", "MetricName"),
+                  summarise, MetricValue=median(MetricValue))
+  }
+  artifacts <- unique(data[[settings@xDimension]])
+  #   artifacts <- sort(artifacts)
+  minValue <- min(data$MetricValue)
+  maxValue <- max(data$MetricValue)
+  plot <- ggplot(data,aes_string(x = settings@xDimension, y = settings@yDimension)) +
+    geom_line(aes_string(group = settings@group, colour=settings@group)) + 
+    geom_point(aes_string(shape = settings@group, colour=settings@group)) + 
+    scale_y_log10(breaks = round(10^seq(log10(minValue), log10(maxValue), by=((log10(maxValue)-log10(minValue))/7)),7), 
+                  labels = round(10^seq(log10(minValue), log10(maxValue), by=((log10(maxValue)-log10(minValue))/7)),2)
+    ) + 
+    ylab(settings@yLabel) +
+    xlab(settings@xLabel) +
+    ggtitle(label = settings@title) +
+    scale_x_log10(breaks = c(artifacts), labels = c(artifacts))
+  
+  ggsave(plot,filename = fileName, width=14, height=7, dpi=192)
+  print(fileName)
 }
--- hu.bme.mit.trainbenchmark.reporting/generate_diagrams.R
+++ hu.bme.mit.trainbenchmark.reporting/generate_diagrams.R
@@ -1,95 +1,56 @@
 library("jsonlite", quietly=T, verbose=F, warn.conflicts=FALSE)
-library("ggplot2",  quietly=T, verbose=F, warn.conflicts=FALSE)
-library("plyr",  quietly=T, verbose=F, warn.conflicts=FALSE)
+library("ggplot2",quietly=T, verbose=F, warn.conflicts=FALSE)
+library("plyr", quietly=T, verbose=F, warn.conflicts=FALSE)
 source("functions.R")
+source("plot.R")
 
 
-# convert the 3-dimensional nested list (results) to 2-dimensional as deleting the unnecessary values
-pre_process <-function(results){ 
-  for (list in 1:length(results)){
-    for(element in 1:length(results[[list]])){
-      if (length(results[[list]][[element]]) > 1){
-          for(value in length(results[[list]][[element]]):2){
-            results[[list]][[element]] <-  results[[list]][[element]][- value] #delete elements except the first
-          }
-        }
-      }
-    }
-  return(results)
-}
 
 
-save_plot <-function(table, column, title, file_name){
-  artifacts <- get_artifacts(table)
-  minValue <- 0
-  maxValue <- 0
-  minValue <- min(table[[column]])
-  maxValue <- max(table[[column]])
-  plot <- ggplot(table,aes_string(x = "artifactSize", y = column)) +
-    geom_line(aes(group = Tools, colour=Tools)) + 
-    geom_point(aes(shape = Tools, colour=Tools)) + 
-    scale_y_log10(breaks = round(10^seq(log10(minValue), log10(maxValue), by=((log10(maxValue)-log10(minValue))/7)),7), 
-                  labels = round(10^seq(log10(minValue), log10(maxValue), by=((log10(maxValue)-log10(minValue))/7)),2)
-                  ) + 
-    ylab("Time (ms)") +
-    xlab("Size") +
-    ggtitle(label = title) +
-    scale_x_log10(breaks = c(artifacts), labels = c(artifacts)) #+
-    #theme(text=element_text(family="Helvetica", size=16),
-    #   panel.background = element_rect(fill="#FFFFFF"),
-    #   panel.grid.major = element_line(size=0.3, colour="#333333"),
-    #   panel.grid.minor = element_line(size=0.15, colour="#CCCCCC"),
-    #   axis.text.x = element_text(colour="black"),
-    #   axis.text.y = element_text(colour="black"),
-    #  legend.position="right"
-    #) #+
-    #geom_text(aes_string(label=column))
-  ggsave(plot,filename = file_name, width=14, height=7, dpi=192)
-  print(file_name)
-}
-
-
-results <-load_results()
-results <- pre_process(results)
-table <- do.call(rbind.data.frame,results)
-table <- change_time_metrics(table, 10**6)
-colnames(table)[which(colnames(table) == "tool")] <- "Tools"
-
-#queries <- unique(table$query)
-scenarios <- unique(table$scenario)
+resultsPath <- "../results/csv/results.csv"
+results <-read.csv(resultsPath, header=TRUE, sep=',')
+configPath <- "./config.json"
+config <- fromJSON(configPath)
 path <- c("../diagrams/")
 if (file.exists(path) == FALSE){
   dir.create(path)
 }
-for(s in scenarios){
-  queries <- unique(subset(table, scenario==s)$query)
-  for(q in queries){
-    
-    subtable <- ddply(table, c("Tools","scenario", "query", "artifactSize"), summarise, 
-                      batchTime = readTime+checkTimes)
-    subtable <- ddply(subtable, c("Tools","scenario", "query", "artifactSize"), summarise, 
-                      batchTime = median(batchTime))
-    subtable <- subset(subtable,query==q & scenario==s)
-    file_name <- paste(path,"BatchValidation-",s,"-",q,".png", sep = "")
-    title <- paste("Batch validation time ",q," (x,y:logscale), ",s, sep = "")
-    save_plot(subtable, c("batchTime"), title, file_name)
-    
-    subtable <-ddply(table, c("Tools","scenario", "query", "artifactSize"), summarise, 
-                     revalidation = checkTimeSum-checkTimes + editTimeSum-editTimes)
-    subtable <-ddply(subtable, c("Tools","scenario", "query", "artifactSize"), summarise, 
-                     revalidation = median(revalidation))
-    subtable <- subset(subtable, query==q & scenario==s)
-    file_name <- paste(path,"Revalidation-",s,"-",q,".png", sep="")
-    title <- paste("Total revalidation time ",q," (x,y:logscale) ",s, sep ="")
-    save_plot(subtable, c("revalidation"), title, file_name)
-    
-    subtable <-ddply(table, c("Tools","scenario", "query", "artifactSize"), summarise, 
-                     totalTime = checkTimeSum + editTimeSum + readTime)
-    subtable <-ddply(subtable, c("Tools","scenario", "query", "artifactSize"), summarise, 
-                     totalTime = median(totalTime))
-    subtable <- subset(subtable, query==q & scenario==s)
-    file_name <- paste(path,"TotalTime-",s,"-",q,".png", sep = "")
-    title <- paste("Total time of the phases ",q," (x,y:logscale) ",s, sep = "")
-    save_plot(subtable, c("totalTime"), title, file_name)
+
+uniqueScenarios <- unique(results$Scenario)
+index <- 0
+settings <- PlotSettings()
+for(func in config$Summarize_Functions){
+  index <- index + 1
+  for(scenario in uniqueScenarios){
+    subData1 <- subset(results, Scenario==scenario & MetricName=="Time")
+    if (config$Dimensions$Groups$Case){
+      uniqueTools <- unique(subData1$Tool)
+      for(tool in uniqueTools){
+        subData2 <- subset(subData1, Tool==tool)
+        
+        if (config$Dimensions$X_Dimensions$Size){
+          title <- paste(tool, ",", scenario, ": ", func, " (Y: Log10) (X: Log10)", sep='')
+          fileName <- paste(path,scenario, "-", tool, "-GroupBy-Case-Function", index, ".", config$Extension, sep='')
+          settings <- setTitle(settings, title)
+          settings <- setDimensions(settings, "Size", "MetricValue")
+          settings <- setGroup(settings, "CaseName")
+          settings <- setLabels(settings, "Size", "Time (ms)")
+          settings <- setAxis(settings, "log10", "log10")
+          savePlot(subData2, settings, func, fileName)
+        }
+        
+        if (config$Dimensions$X_Dimensions$Iteration == TRUE){
+          
+        } 
+      }
+    }
+    if (config$Dimensions$Groups$Tool == TRUE){
+      if (config$Dimensions$X_Dimensions$Size == TRUE){
+        
+      }
+      if (config$Dimensions$X_Dimensions$Iteration == TRUE){
+        
+      }     
+    }
   }
 }
