b59407f9db2163d92d51896a2a58fa39aa128fca
ZsoltKovari <kovari.zsolt91@gmail.com>
2015-03-11 11:24:25 +0100

    Change legend position on diagrams

--- hu.bme.mit.trainbenchmark.reporting/theme.R
+++ hu.bme.mit.trainbenchmark.reporting/theme.R
@@ -1,5 +1,7 @@
 bwTheme <- theme(text=element_text(family="Helvetica", size=16),
                  panel.background = element_rect(fill="#FFFFFF"),
+                 legend.position="bottom",
+                 legend.direction="vertical",
                  panel.grid.major = element_line(size=0.3, colour="#333333"),
                  panel.grid.minor = element_line(size=0.15, colour="#CCCCCC"),
                  axis.text.x = element_text(colour="black"),
