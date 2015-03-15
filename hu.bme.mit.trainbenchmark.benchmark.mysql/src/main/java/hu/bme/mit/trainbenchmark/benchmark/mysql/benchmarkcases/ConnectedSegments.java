db70358581acf10f972c8bfffadbec241a4bc1d4
ZsoltKovari <kovari.zsolt91@gmail.com>
2015-03-15 17:32:41 +0100

    ConnectedSegments Batch implementation of MySQL

---
+++ hu.bme.mit.trainbenchmark.benchmark.mysql/src/main/java/hu/bme/mit/trainbenchmark/benchmark/mysql/benchmarkcases/ConnectedSegments.java
@@ -0,0 +1,10 @@
+package hu.bme.mit.trainbenchmark.benchmark.mysql.benchmarkcases;
+
+public class ConnectedSegments extends MySQLBenchmarkCase {
+
+	@Override
+	public String getName() {
+		return "ConnectedSegments";
+	}
+	
+}
