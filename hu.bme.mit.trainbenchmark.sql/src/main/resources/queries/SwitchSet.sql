1484ed50a8db7292ae146a1f50af371a91b4e941
Gabor Szarnyas <szarnyasg@gmail.com>
2015-06-19 12:53:19 +0200

    CS fixed

--- hu.bme.mit.trainbenchmark.sql/src/main/resources/queries/SwitchSet.sql
+++ hu.bme.mit.trainbenchmark.sql/src/main/resources/queries/SwitchSet.sql
@@ -1,7 +1,11 @@
-SELECT Semaphore.id AS semaphore, Route.id AS route, SwitchPosition.id AS swP, Switch.id AS sw
+SELECT
+	Semaphore.id AS semaphore,
+	Route.id AS route,
+	SwitchPosition.id AS swP,
+	Switch.id AS sw
 FROM SwitchPosition, Route, Semaphore, Switch 
 WHERE Route.entry = Semaphore.id 
   AND Route.id = SwitchPosition.follows
   AND SwitchPosition.switch = Switch.id 
   AND Switch.currentPosition != SwitchPosition.position 
-  AND Semaphore.signal = 2
+  AND Semaphore.signal = 2;
