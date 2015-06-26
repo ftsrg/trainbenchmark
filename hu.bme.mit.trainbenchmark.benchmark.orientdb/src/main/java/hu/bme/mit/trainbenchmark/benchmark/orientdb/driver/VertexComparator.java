62d76810e92fcb4aab6c5fa29f0022794eeceed9
Zoltán Selmeczi <selmeczi.z@gmail.com>
2015-06-26 16:57:20 +0200

    Fixing vertex comparator

--- hu.bme.mit.trainbenchmark.benchmark.orientdb/src/main/java/hu/bme/mit/trainbenchmark/benchmark/orientdb/driver/VertexComparator.java
+++ hu.bme.mit.trainbenchmark.benchmark.orientdb/src/main/java/hu/bme/mit/trainbenchmark/benchmark/orientdb/driver/VertexComparator.java
@@ -7,8 +7,8 @@ import com.tinkerpop.blueprints.Vertex;
 public class VertexComparator implements Comparator<Vertex> {
 
 	public int compare(Vertex vertex1, Vertex vertex2) {
-		final String id1 = vertex1.getId().toString();
-		final String id2 = vertex2.getId().toString();
-		return id1.compareTo(id2);
+		final long id1 = Long.parseLong(vertex1.getId().toString().split(":")[1]);
+		final long id2 = Long.parseLong(vertex2.getId().toString().split(":")[1]);
+		return Long.compare(id1, id2);
 	}
 }
