349d00e0a9524aa18a0c02d263a6880e4c71625c
Gabor Szarnyas <szarnyasg@gmail.com>
2015-06-26 16:53:03 +0200

    Some OCL progress -- PosLength query prototype working

--- hu.bme.mit.trainbenchmark.benchmark.eclipseocl/src/main/java/hu/bme/mit/trainbenchmark/benchmark/eclipseocl/checkers/EclipseOCLChecker.java
+++ hu.bme.mit.trainbenchmark.benchmark.eclipseocl/src/main/java/hu/bme/mit/trainbenchmark/benchmark/eclipseocl/checkers/EclipseOCLChecker.java
@@ -14,38 +14,44 @@ package hu.bme.mit.trainbenchmark.benchmark.eclipseocl.checkers;
 import hu.bme.mit.trainbenchmark.benchmark.checker.Checker;
 import hu.bme.mit.trainbenchmark.benchmark.config.BenchmarkConfig;
 import hu.bme.mit.trainbenchmark.benchmark.util.Util;
+import hu.bme.mit.trainbenchmark.emf.EMFDriver;
 import hu.bme.mit.trainbenchmark.emf.matches.EMFMatch;
 import hu.bme.mit.trainbenchmark.railway.RailwayContainer;
 import hu.bme.mit.trainbenchmark.railway.RailwayPackage;
+import hu.bme.mit.trainbenchmark.railway.Segment;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Set;
 
-import org.eclipse.emf.common.util.TreeIterator;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.ocl.ParserException;
 import org.eclipse.ocl.ecore.OCL;
 import org.eclipse.ocl.ecore.OCL.Helper;
+import org.eclipse.ocl.ecore.OCL.Query;
 import org.eclipse.ocl.ecore.OCLExpression;
 
-public class EclipseOCLChecker extends  Checker<EMFMatch> {
-	
+public class EclipseOCLChecker extends Checker<EMFMatch> {
+
 	protected Collection<EMFMatch> matches;
 	protected OCL ocl;
-	protected OCLExpression query;
+	protected Query queryEvaluator;
 	protected RailwayContainer container;
+	protected EMFDriver driver;
 
-	
-	public EclipseOCLChecker(final BenchmarkConfig bc) throws IOException {
-		ocl = OCL.newInstance();
-		final String oclConstraint = Util.readFile(bc.getWorkspacePath()
+	public EclipseOCLChecker(final EMFDriver driver, final BenchmarkConfig bc) throws IOException {
+		super();
+		this.driver = driver;
+
+		final String oclQuery = Util.readFile(bc.getWorkspacePath()
 				+ "/hu.bme.mit.trainbenchmark.benchmark.eclipseocl/src/main/resources/queries/" + bc.getQuery() + ".ocl");
 
+		ocl = OCL.newInstance();
 		final Helper helper = ocl.createOCLHelper();
 		helper.setContext(RailwayPackage.eINSTANCE.getSegment());
 		try {
-			query = helper.createQuery(oclConstraint);
+			final OCLExpression query = helper.createQuery(oclQuery);
+			queryEvaluator = ocl.createQuery(query);
 		} catch (final ParserException e) {
 			throw new IOException(e);
 		}
@@ -55,15 +61,13 @@ public class EclipseOCLChecker extends  Checker<EMFMatch> {
 	public Collection<EMFMatch> check() {
 		matches = new ArrayList<>();
 
-		final TreeIterator<EObject> contents = container.eAllContents();
-		while (contents.hasNext()) {
-			final EObject eObject = contents.next();
-
-			final Object result = ocl.evaluate(eObject, query);
-			if (result == Boolean.TRUE) {
-				matches.add(null);
-			}
-		}
+//		final TreeIterator<EObject> contents = driver.getContainer().eAllContents();
+		final Object evaluate = queryEvaluator.evaluate(driver.getContainer());
+		System.out.println(evaluate);
+		System.out.println(evaluate.getClass());
+		final Set<Segment> results = (Set<Segment>) evaluate;
+		System.out.println(results.size());
+		
 
 		return matches;
 	}
