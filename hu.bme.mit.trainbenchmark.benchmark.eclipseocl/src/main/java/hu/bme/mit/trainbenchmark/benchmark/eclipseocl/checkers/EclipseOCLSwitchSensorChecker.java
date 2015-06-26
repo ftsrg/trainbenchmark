6d92d0e88967ed2c42fde4f43f235268ed45ea0b
Gabor Szarnyas <szarnyasg@gmail.com>
2015-06-26 17:37:35 +0200

    OCL PosLength and SwitchSensor queries working

---
+++ hu.bme.mit.trainbenchmark.benchmark.eclipseocl/src/main/java/hu/bme/mit/trainbenchmark/benchmark/eclipseocl/checkers/EclipseOCLSwitchSensorChecker.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2015, Benedek Izso, Gabor Szarnyas, Istvan Rath and Daniel Varro
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Benedek Izso - initial API and implementation
+ *   Gabor Szarnyas - initial API and implementation
+ *******************************************************************************/
+package hu.bme.mit.trainbenchmark.benchmark.eclipseocl.checkers;
+
+import hu.bme.mit.trainbenchmark.benchmark.config.BenchmarkConfig;
+import hu.bme.mit.trainbenchmark.emf.EMFDriver;
+import hu.bme.mit.trainbenchmark.emf.matches.EMFSwitchSensorMatch;
+import hu.bme.mit.trainbenchmark.railway.RailwayPackage;
+import hu.bme.mit.trainbenchmark.railway.Switch;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Set;
+
+import org.eclipse.emf.ecore.EClassifier;
+
+public class EclipseOCLSwitchSensorChecker extends EclipseOCLChecker<EMFSwitchSensorMatch> {
+
+	public EclipseOCLSwitchSensorChecker(final EMFDriver driver, final BenchmarkConfig bc) throws IOException {
+		super(driver, bc);
+	}
+
+	@Override
+	protected EClassifier getContext() {
+		return RailwayPackage.eINSTANCE.getSegment();
+	}
+
+	@Override
+	public Collection<EMFSwitchSensorMatch> check() {
+		matches = new ArrayList<>();
+
+		final Set<Switch> results = (Set<Switch>) queryEvaluator.evaluate(driver.getContainer());
+		for (final Switch sw : results) {
+			matches.add(new EMFSwitchSensorMatch(sw));
+		}
+
+		return matches;
+	}
+
+}
