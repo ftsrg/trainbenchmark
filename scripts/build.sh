e78efaa557f6ca5432dd8ac7aa365f474e02240f
Gabor Szarnyas <szarnyasg@gmail.com>
2015-06-26 21:56:58 +0200

    Eclipse OCL working

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,5 +6,5 @@ export MAVEN_OPTS="-Xmx2G"
 
 ./scripts/build-artifacts.sh
 
-mvn clean install -P core,emf,graph,rdf,sql,java,drools,emfincquery,neo4j,jena,sesame,mysql --fail-at-end
+mvn clean install -P core,emf,graph,rdf,sql,java,drools,emfincquery,neo4j,jena,sesame,mysql,eclipseocl --fail-at-end
 #,memsql,allegro,virtuoso
