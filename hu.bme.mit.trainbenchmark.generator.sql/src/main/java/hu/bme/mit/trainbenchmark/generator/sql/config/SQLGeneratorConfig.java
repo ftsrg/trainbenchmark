b53c8a63b0050a185dbdbd19cdb8af22e10c3612
Gabor Szarnyas <szarnyasg@gmail.com>
2015-06-29 10:14:26 +0200

    OrientDB added and working

--- hu.bme.mit.trainbenchmark.generator.sql/src/main/java/hu/bme/mit/trainbenchmark/generator/sql/config/SQLGeneratorConfig.java
+++ hu.bme.mit.trainbenchmark.generator.sql/src/main/java/hu/bme/mit/trainbenchmark/generator/sql/config/SQLGeneratorConfig.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010-2014, Benedek Izso, Gabor Szarnyas, Istvan Rath and Daniel Varro
+ * Copyright (c) 2010-2015, Benedek Izso, Gabor Szarnyas, Istvan Rath and Daniel Varro
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -9,16 +9,15 @@
  *   Benedek Izso - initial API and implementation
  *   Gabor Szarnyas - initial API and implementation
  *******************************************************************************/
-
-package hu.bme.mit.trainbenchmark.generator.emf.config;
+package hu.bme.mit.trainbenchmark.generator.sql.config;
 
 import hu.bme.mit.trainbenchmark.generator.config.GeneratorConfig;
 
 import org.apache.commons.cli.ParseException;
 
-public class EMFGeneratorConfig extends GeneratorConfig {
+public class SQLGeneratorConfig extends GeneratorConfig {
 
-	public EMFGeneratorConfig(final String[] args) throws ParseException {
+	public SQLGeneratorConfig(final String[] args) throws ParseException {
 		super(args);
 	}
 
