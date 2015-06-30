449310cd9622ccd313bad4628c63c4a2c278fed1
Gabor Szarnyas <szarnyasg@gmail.com>
2015-06-30 11:59:19 +0200

    Removed analyze project

--- hu.bme.mit.trainbenchmark.analyze/install.R
+++
@@ -1,7 +0,0 @@
-install.packages("knitr", repos="http://cran.rstudio.com/");
-install.packages("ggplot2", repos="http://cran.rstudio.com/");
-install.packages("scales", repos="http://cran.rstudio.com/");
-install.packages("xtable", repos="http://cran.rstudio.com/");
-install.packages("reshape", repos="http://cran.rstudio.com/");
-install.packages("extrafont", repos="http://cran.rstudio.com/");
-install.packages("fontcm", repos="http://cran.rstudio.com/");
