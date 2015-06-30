847fdc9025f1003116e4bb61bd659468feadf3ce
Gabor Szarnyas <szarnyasg@gmail.com>
2015-06-30 12:09:46 +0200

    Removed skipTests

--- scripts/build-travis.sh
+++ scripts/build-travis.sh
@@ -3,5 +3,5 @@
 cd "$( cd "$( dirname "$0" )" && pwd )/.."
 
 #echo Running quiet build.
-time mvn clean install -P core,emf,graph,rdf,sql,drools,eclipseocl,emfincquery,java,jena,mysql,neo4j,orientdb,sesame -DskipTests || exit 1
+time mvn clean install -P core,emf,graph,rdf,sql,drools,eclipseocl,emfincquery,java,jena,mysql,neo4j,orientdb,sesame || exit 1
 #echo Build finished.
