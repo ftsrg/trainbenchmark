04f7e51f7c8f6d6f4484307e1faf3d112ba58a6a
Gabor Szarnyas <szarnyasg@gmail.com>
2015-06-30 17:22:09 +0200

    Cleanup & fixed copyrights

--- hu.bme.mit.trainbenchmark.config/src/main/java/hu/bme/mit/trainbenchmark/constants/Scenario.java
+++ hu.bme.mit.trainbenchmark.config/src/main/java/hu/bme/mit/trainbenchmark/constants/Scenario.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010-2015, Benedek Izso, Gabor Szarnyas, Istvan Rath and Daniel VarroScenario
+ * Copyright (c) 2010-2015, Benedek Izso, Gabor Szarnyas, Istvan Rath and Daniel Varro
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
