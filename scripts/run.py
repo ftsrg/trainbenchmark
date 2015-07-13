358bf1618e83b4a954ecabcb633e490af0531658
Gabor Szarnyas <szarnyasg@gmail.com>
2015-07-13 21:29:10 +0200

    Updated Python script, generator/measure works for most cases

--- scripts/run.py
+++ scripts/run.py
@@ -16,37 +16,77 @@ import os
 import subprocess
 import sys
 import argparse
-import logging
 
 import util
-import benchmark
-from generate import Generator
 from loader import Loader
 import deps
-import log
+import targets
+
 
 def flatten(lst):
     return sum(([x] if not isinstance(x, list) else flatten(x) for x in lst), [])
 
 
-def build(configurations, skip_tests):
+def build(formats, tools, skip_tests):
     """Build the projects.
     
     @param configurations: a list of Configuration objects
     @param skip_tests: skip JUnit tests if given
     """
     profiles = {"core"}
-    for tool in config.tools:
-        profiles.add(tool)
-        profiles.add(loader.get_dependency(tool))
+    profiles = profiles.union(formats)
+    profiles = profiles.union(tools)
 
     profiles_arg = ",".join(profiles)
-    print(profiles_arg)
 
     cmd = ["mvn", "clean", "install", "-P", profiles_arg]
     if skip_tests:
         cmd.append("-DskipTests")
     subprocess.call(cmd)
+
+
+def generate(formats, scenarios, sizes):
+    """
+    Generates the models after the configurations parameter.
+    """
+
+    for scenario in scenarios:
+        for format in formats:
+            path = "./hu.bme.mit.trainbenchmark.generator.{FORMAT}/".format(FORMAT=format)
+            util.set_working_directory(path)
+            target = targets.get_generator_jar(format)
+            for size in sizes:
+                print("Generate model: <format: " + format +
+                      ", scenario: " + scenario +
+                      ", size: " + str(size) + ">")
+                subprocess.call(["java", "-Xmx" + java_xmx,
+                                 "-jar", target,
+                                 "-scenario", scenario,
+                                 "-size", str(size)])
+            util.set_working_directory("..")
+
+
+def measure(tools, scenarios, sizes, queries):
+    """Benchmark function.
+    """
+    for tool in tools:
+        path = "./hu.bme.mit.trainbenchmark.benchmark.{TOOL}/".format(TOOL=tool)
+        util.set_working_directory(path)
+        target = targets.get_tool_jar(tool)
+
+        for scenario in scenarios:
+            for size in sizes:
+                for query in queries:
+                    print("Run benchmark: <tool: " + tool +
+                          ", scenario: " + scenario +
+                          ", query: " + query +
+                          ", size: " + str(size) + ">")
+                    subprocess.call(["java", "-Xmx" + java_xmx, "-jar", target,
+                                     "-scenario", scenario,
+                                     "-query", query,
+                                     "-size", str(size)
+                                     ])
+        util.set_working_directory("..")
 
 
 if __name__ == "__main__":
@@ -65,25 +105,27 @@ if __name__ == "__main__":
                         action="store_true")
     args = parser.parse_args()
 
-    log.init_log()
     # set working directory to this file's path
-
     util.set_working_directory()
-
-    logging.info("Main module: run.py.")
     loader = Loader()
     config = loader.load()
+
+    formats = set()
+    for tool in config.tools:
+        formats.add(loader.get_format(tool))
+
+    java_xmx = "2g"
 
     # if there are no args, execute a full sequence
     # with the test and the visualization/reporting
     no_args = all(val is False for val in vars(args).values())
     if no_args:
-        args.test = True
+        args.build = True
+        args.generate = True
+        args.measure = True
     if args.build:
-        build(config, args.skip_tests)
+        build(formats, config.tools, args.skip_tests)
     if args.generate:
-        pass
+        generate(formats, config.scenarios, config.sizes)
     if args.measure:
-        pass
-    if args.test:
-        build(config, True)
+        measure(config.tools, config.scenarios, config.sizes, config.queries)
