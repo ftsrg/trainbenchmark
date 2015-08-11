675c41675a9e666644e6423e69caa1ce052c491f
Gabor Szarnyas <szarnyasg@gmail.com>
2015-08-11 15:12:31 +0200

    Refactoring scripts

--- scripts/resolve-dependencies.sh
+++ scripts/resolve-dependencies.sh
@@ -2,13 +2,6 @@
 
 cd "$( cd "$( dirname "$0" )" && pwd )/../.."
 
-if [ ! -d neo4j-shell-tools ]; then
-  git clone https://github.com/jexp/neo4j-shell-tools
-fi
-cd neo4j-shell-tools
-mvn clean install -DskipTests
-cd ..
-
 if [ ! -d mondo-sam ]; then
   git clone https://github.com/FTSRG/mondo-sam.git
 fi
