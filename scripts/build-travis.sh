ad663e2074a1bf6eb097dc356cf7a745717581d0
Gabor Szarnyas <szarnyasg@gmail.com>
2015-08-11 14:30:49 +0200

    Removed unused scripts

--- scripts/build-travis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd "$( cd "$( dirname "$0" )" && pwd )/.."
-
-#echo Running quiet build.
-time mvn clean install -P core,emf,graph,rdf,sql,drools,eclipseocl,emfincquery,emfapi,jena,mysql,neo4j,sesame || exit 1
-# orientdb
-#echo Build finished.
