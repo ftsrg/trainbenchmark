6e07c5a4178cc31ba8693065b50380542fc429cb
Gabor Szarnyas <szarnyasg@gmail.com>
2015-08-11 15:05:09 +0200

    Updated scripts, WIP

--- scripts/loc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd "$( cd "$( dirname "$0" )" && pwd )/.."
-
-# to install cloc, use: sudo apt-get install cloc
-cloc --match-f '.*\.java' --exclude-dir hu.bme.mit.trainbenchmark.emf.model,hu.bme.mit.trainbenchmark.benchmark.emfincquery.patterns/ .
