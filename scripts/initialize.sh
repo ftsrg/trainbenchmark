290c22997fe21a65d4e5a9713fada147081675d5
Gabor Szarnyas <szarnyasg@gmail.com>
2015-08-11 15:37:42 +0200

    Moved requirements

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -4,4 +4,4 @@ cd "$( cd "$( dirname "$0" )" && pwd )"
 
 sudo apt-get install python3-setuptools
 sudo easy_install3 pip
-sudo pip3 install -r requirements.txt
+sudo pip3 install -r ../requirements.txt
