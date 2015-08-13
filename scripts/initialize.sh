28006ee94b578e02da35cc1aefb6efe508ad1e61
Gabor Szarnyas <szarnyasg@gmail.com>
2015-08-13 20:48:29 +0200

    Added -y to intall command

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -2,6 +2,6 @@
 
 cd "$( cd "$( dirname "$0" )" && pwd )"
 
-sudo apt-get install python3-setuptools
+sudo apt-get install -y python3-setuptools
 sudo easy_install3 pip
 sudo pip3 install -r ../requirements.txt
