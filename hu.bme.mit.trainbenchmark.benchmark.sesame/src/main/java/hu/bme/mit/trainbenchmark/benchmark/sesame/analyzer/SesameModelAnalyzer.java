35c6a0e72ace26f9eee5c0c8667193f613917d09
ZsoltKovari <kovari.zsolt91@gmail.com>
2015-08-19 09:42:20 +0200

    Calculate clustering coefficient for Sesame

--- hu.bme.mit.trainbenchmark.benchmark.sesame/src/main/java/hu/bme/mit/trainbenchmark/benchmark/sesame/analyzer/SesameModelAnalyzer.java
+++ hu.bme.mit.trainbenchmark.benchmark.sesame/src/main/java/hu/bme/mit/trainbenchmark/benchmark/sesame/analyzer/SesameModelAnalyzer.java
@@ -12,10 +12,18 @@
 
 package hu.bme.mit.trainbenchmark.benchmark.sesame.analyzer;
 
+import static hu.bme.mit.trainbenchmark.constants.schedule.ScheduleConstants.STATION;
 import hu.bme.mit.trainbenchmark.benchmark.rdf.analyzer.RDFModelAnalyzer;
 import hu.bme.mit.trainbenchmark.benchmark.sesame.driver.SesameDriver;
 import hu.bme.mit.trainbenchmark.constants.EdgeDirection;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.model.vocabulary.RDF;
 import org.openrdf.query.BindingSet;
 import org.openrdf.query.MalformedQueryException;
 import org.openrdf.query.QueryEvaluationException;
@@ -24,8 +32,12 @@ import org.openrdf.query.TupleQuery;
 import org.openrdf.query.TupleQueryResult;
 import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
+import org.openrdf.repository.RepositoryResult;
 
 public class SesameModelAnalyzer extends RDFModelAnalyzer<SesameDriver> {
+
+	protected ValueFactory vf;
+	protected RepositoryConnection connection;
 
 	public SesameModelAnalyzer(SesameDriver driver) {
 		super(driver);
@@ -33,10 +45,10 @@ public class SesameModelAnalyzer extends RDFModelAnalyzer<SesameDriver> {
 
 	@Override
 	public void calculateMetrics() {
-		RepositoryConnection connection = driver.getConnection();
+		connection = driver.getConnection();
+		vf = driver.getValueFactory();
 		try {
-			TupleQuery query = connection.prepareTupleQuery(QueryLanguage.SPARQL,
-					getTriples);
+			TupleQuery query = connection.prepareTupleQuery(QueryLanguage.SPARQL, getTriples);
 			TupleQueryResult results = query.evaluate();
 
 			calculateEdges(results);
@@ -54,13 +66,15 @@ public class SesameModelAnalyzer extends RDFModelAnalyzer<SesameDriver> {
 
 	}
 
-	private void calculateDegrees(TupleQueryResult results) throws QueryEvaluationException {
+	private void calculateDegrees(TupleQueryResult results) throws QueryEvaluationException,
+			RepositoryException, MalformedQueryException {
 		double degree = 0;
 		BindingSet set;
 
 		while (results.hasNext()) {
 			set = results.next();
 			degree = Double.parseDouble(set.getValue("outdegree").stringValue());
+			determineClustering(vf.createURI(set.getValue("x").stringValue()));
 			numberOfNodesWithOutgoingDegrees += degree > 0 ? 1 : 0;
 			numberOfNodes++;
 
@@ -73,6 +87,43 @@ public class SesameModelAnalyzer extends RDFModelAnalyzer<SesameDriver> {
 		calculateAverageDegree(EdgeDirection.OUTGOING);
 	}
 
+	private void determineClustering(final URI subject) throws RepositoryException,
+			MalformedQueryException, QueryEvaluationException {
+		String queryString = buildNeighborQuery(subject);
+		TupleQuery query = connection.prepareTupleQuery(QueryLanguage.SPARQL, queryString);
+		TupleQueryResult result = query.evaluate();
+
+		Set<String> neighbors = new HashSet<>();
+		while (result.hasNext()) {
+			neighbors.add(result.next().getValue("o").stringValue());
+		}
+
+		if (neighbors.size() == 0) {
+			return;
+		}
+
+		int connected = 0;
+		for (String n : neighbors) {
+			result = connection.prepareTupleQuery(QueryLanguage.SPARQL,
+					buildNeighborQuery(vf.createURI(n))).evaluate();
+			while (result.hasNext()) {
+				if (neighbors.contains(result.next().getValue("o").stringValue())) {
+					connected++;
+				}
+			}
+		}
+		URI clazz;
+		RepositoryResult<Statement> types = connection.getStatements(subject, RDF.TYPE, null, false);
+		while (types.hasNext()) {
+			if (cutPrefix(types.next().getObject().stringValue()).equals(STATION)) {
+				addClusteringCoefficient(connected, neighbors.size(), STATION);
+				return;
+			}
+		}
+		addClusteringCoefficient(connected, neighbors.size());
+
+	}
+
 	private void calculateEdges(TupleQueryResult results) throws QueryEvaluationException {
 		BindingSet set;
 		while (results.hasNext()) {
@@ -81,8 +132,18 @@ public class SesameModelAnalyzer extends RDFModelAnalyzer<SesameDriver> {
 		}
 	}
 
-	private void calculateNumberOfDegrees(final TupleQueryResult results)
-			throws QueryEvaluationException {
+	protected String buildNeighborQuery(final URI subjectURI) {
+		String queryString = String
+				.format("PREFIX  base: <http://www.semanticweb.org/ontologies/2015/trainbenchmark#> "
+						+ "PREFIX  rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+						+ "SELECT  ?o "
+						+ "WHERE "
+						+ "{ { base:%s ?p ?o FILTER ( ( ! isLiteral(?o) ) && ( ?p != rdf:type ) )}}",
+						cutPrefix(subjectURI.stringValue()));
+		return queryString;
+	}
+
+	private void calculateNumberOfDegrees(final TupleQueryResult results) throws QueryEvaluationException {
 		BindingSet set;
 		double degree = 0;
 		int roundedDegree = roundAverageDegree(EdgeDirection.BOTH);
@@ -95,6 +156,10 @@ public class SesameModelAnalyzer extends RDFModelAnalyzer<SesameDriver> {
 			degree += Double.parseDouble(set.getValue("indegree").stringValue());
 			changeNumberOfDegrees(EdgeDirection.BOTH, degree, roundedDegree);
 		}
+	}
+
+	protected String cutPrefix(final String uri) {
+		return uri.substring(uri.indexOf("#") + 1);
 	}
 
 }
