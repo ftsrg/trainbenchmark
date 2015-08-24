9133aa032bc0eeb81572ae0de9ba3af6942fde3b
Gabor Szarnyas <szarnyasg@gmail.com>
2015-08-24 09:01:21 +0200

    Merged scripts

--- scripts/report.sh
+++ scripts/report.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+
 cd "$( cd "$( dirname "$0" )" && pwd )"
 
-python3 ../../mondo-sam/reporting/report.py --source ../../trainbenchmark/results/results.csv \
---output ../../trainbenchmark/diagrams/ --config ../../trainbenchmark/config/reporting.json
+../../mondo-sam/reporting/convert_results.py --source ../../trainbenchmark/results/json/ --jsonfile ../../trainbenchmark/results/results.json --csvfile ../../trainbenchmark/results/results.csv
+python3 ../../mondo-sam/reporting/report.py --source ../../trainbenchmark/results/results.csv --output ../../trainbenchmark/diagrams/ --config ../../trainbenchmark/config/reporting.json
