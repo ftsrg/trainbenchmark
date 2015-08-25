da15235e4ac6bc4cafe313a9cef77c2260ea2bd2
Gabor Szarnyas <szarnyasg@gmail.com>
2015-08-25 20:49:33 +0200

    Added branch selection for MONDO-SAM

--- scripts/dep-mondo-sam.sh
+++ scripts/dep-mondo-sam.sh
@@ -3,7 +3,7 @@
 cd "$( cd "$( dirname "$0" )" && pwd )/../.."
 
 if [ ! -d mondo-sam ]; then
-  git clone https://github.com/FTSRG/mondo-sam.git
+  git clone --branch issue-9 https://github.com/FTSRG/mondo-sam.git
 fi
 cd mondo-sam
 mvn clean install -DskipTests
