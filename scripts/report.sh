d8cf9b9350fcf5363fb207ba02fb0d84f62bb532
Gabor Szarnyas <szarnyasg@gmail.com>
2015-09-01 13:55:51 +0200

    Separated convert & report scripts

--- scripts/report.sh
+++ scripts/report.sh
@@ -2,5 +2,4 @@
 
 cd "$( cd "$( dirname "$0" )" && pwd )"
 
-../../mondo-sam/reporting/convert_results.py --source ../../trainbenchmark/results/json/ --jsonfile ../../trainbenchmark/results/results.json --csvfile ../../trainbenchmark/results/results.csv
 python3 ../../mondo-sam/reporting/report.py --source ../../trainbenchmark/results/results.csv --output ../../trainbenchmark/diagrams/ --config ../../trainbenchmark/config/reporting.json
