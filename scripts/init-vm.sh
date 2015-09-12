ee045d0af70aa01892d06aa63f26ece2eff8ec4a
ZsoltKovari <kovari.zsolt91@gmail.com>
2015-09-12 14:08:23 +0200

    Init-VM script

---
+++ scripts/init-vm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get install -y git
+sudo apt-get install -y maven
+
+sudo apt-get install -y software-properties-common
+sudo bash -c "echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections"
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install -y oracle-java8-installer
