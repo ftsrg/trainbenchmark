e2cab03674a6c6822562525e9fbce366f19ed314
János Maginecz <wafleeee@gmail.com>
2015-09-13 14:19:31 +0200

    remove update

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -2,7 +2,6 @@
 
 cd "$( cd "$( dirname "$0" )" && pwd )"
 
-sudo apt-get update
 sudo apt-get install -y python3 python3-setuptools
 sudo easy_install3 pip
 sudo pip3 install -r ../requirements.txt
