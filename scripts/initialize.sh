541869327fe37b1ce2e876007dc795b2f2adfb6a
János Maginecz <wafleeee@gmail.com>
2015-09-13 15:38:45 +0200

    install python3-dev

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -3,7 +3,7 @@
 cd "$( cd "$( dirname "$0" )" && pwd )"
 
 sudo apt-get -y update
-sudo apt-get install -y python3 python3-setuptools
+sudo apt-get install -y python3 python3-setuptools python3-dev
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
 sudo pip3 install -r ../requirements.txt
