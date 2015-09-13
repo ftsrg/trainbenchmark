ab634246bebdcf9f2717cadab182b3bc179e0854
János Maginecz <wafleeee@gmail.com>
2015-09-13 15:34:58 +0200

    install python3-setuptools

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -3,7 +3,7 @@
 cd "$( cd "$( dirname "$0" )" && pwd )"
 
 sudo apt-get -y update
-sudo apt-get install -y python3
+sudo apt-get install -y python3 python3-setuptools
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
 sudo pip3 install -r ../requirements.txt
