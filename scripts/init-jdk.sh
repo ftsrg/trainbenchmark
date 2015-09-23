ef6811921f70557dceb536d184a04ff8ef94214a
Gabor Szarnyas <szarnyasg@gmail.com>
2015-09-23 14:45:26 +0100

    Added more initialization scripts

---
+++ scripts/init-jdk.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get install -y software-properties-common && sudo bash -c "echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections" && sudo add-apt-repository -y ppa:webupd8team/java && sudo apt-get update && sudo apt-get install -y oracle-java8-installer
