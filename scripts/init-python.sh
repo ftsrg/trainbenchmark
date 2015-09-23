a129f774bb0e93982a445be39b853e0277d77810
Gabor Szarnyas <szarnyasg@gmail.com>
2015-09-23 16:20:33 +0100

    Moved requirements.txt

--- scripts/init-python.sh
+++ scripts/init-python.sh
@@ -6,4 +6,4 @@ sudo apt-get -y update
 sudo apt-get install -y python3 python3-setuptools python3-dev
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
-sudo pip3 install -r ../requirements.txt
+sudo pip3 install -r ../config/requirements.txt
