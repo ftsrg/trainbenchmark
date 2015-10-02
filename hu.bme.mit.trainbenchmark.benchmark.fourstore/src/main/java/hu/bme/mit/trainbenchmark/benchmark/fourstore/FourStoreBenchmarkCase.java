bcab7b3f55d71efd956d0257368873f3b0a9ba64
Gabor Szarnyas <szarnyasg@gmail.com>
2015-10-02 12:03:41 +0100

    FourStore updates (not compiling)

--- hu.bme.mit.trainbenchmark.benchmark.fourstore/src/main/java/hu/bme/mit/trainbenchmark/benchmark/fourstore/FourStoreBenchmarkCase.java
+++ hu.bme.mit.trainbenchmark.benchmark.fourstore/src/main/java/hu/bme/mit/trainbenchmark/benchmark/fourstore/FourStoreBenchmarkCase.java
@@ -9,41 +9,42 @@
  *   Benedek Izso - initial API and implementation
  *   Gabor Szarnyas - initial API and implementation
  *******************************************************************************/
-package hu.bme.mit.trainbenchmark.benchmark.fourstore.benchmarkcases;
+package hu.bme.mit.trainbenchmark.benchmark.fourstore;
 
 import java.io.IOException;
 import java.util.Comparator;
 
 import org.openrdf.model.URI;
 
-import hu.bme.mit.trainbenchmark.benchmark.benchmarkcases.AbstractBenchmarkCase;
+import hu.bme.mit.trainbenchmark.benchmark.benchmarkcases.AbstractBenchmarkCaseRunner;
+import hu.bme.mit.trainbenchmark.benchmark.benchmarkcases.transformations.Transformation;
+import hu.bme.mit.trainbenchmark.benchmark.fourstore.benchmarkcases.FourStoreChecker;
 import hu.bme.mit.trainbenchmark.benchmark.fourstore.config.FourStoreBenchmarkConfig;
 import hu.bme.mit.trainbenchmark.benchmark.fourstore.driver.FourStoreDriver;
 import hu.bme.mit.trainbenchmark.benchmark.fourstore.transformations.FourStoreTransformation;
 import hu.bme.mit.trainbenchmark.benchmark.sesame.matches.SesameMatch;
 import hu.bme.mit.trainbenchmark.benchmark.sesame.matches.SesameMatchComparator;
 
-public class FourStoreBenchmarkCase extends AbstractBenchmarkCase<SesameMatch, URI> {
+public class FourStoreBenchmarkCase extends AbstractBenchmarkCaseRunner<SesameMatch, URI, FourStoreDriver> {
 
 	protected FourStoreDriver fourStoreDriver;
 
 	@Override
 	public void initialize() throws IOException {
-		super.initialize();
-
 		final FourStoreBenchmarkConfig fsbc = (FourStoreBenchmarkConfig) bc;
 
-		driver = fourStoreDriver = new FourStoreDriver(rdfbc);
+		driver = new FourStoreDriver(fsbc);
 		checker = new FourStoreChecker(fourStoreDriver, fsbc);
-
-		if (bc.getScenario().hasTranformation()) {
-			transformation = FourStoreTransformation.newInstance(fourStoreDriver, bc.getQuery(), bc.getScenario());
-		}
 	}
 
 	@Override
 	protected Comparator<?> getMatchComparator() {
 		return new SesameMatchComparator();
+	}
+
+	@Override
+	protected Transformation<?> getTransformation() throws IOException {
+		return FourStoreTransformation.newInstance(fourStoreDriver, bc.getQuery(), bc.getScenario());
 	}
 
 }
