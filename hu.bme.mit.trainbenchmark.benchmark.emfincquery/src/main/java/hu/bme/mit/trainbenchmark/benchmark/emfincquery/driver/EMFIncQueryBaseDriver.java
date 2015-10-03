082086e0253229c48a32c3a20fc2635eea37b24f
Gabor Szarnyas <szarnyasg@gmail.com>
2015-10-03 14:34:39 +0100

    Added optimisation for IncQuery to use the Base Indexer

--- hu.bme.mit.trainbenchmark.benchmark.emfincquery/src/main/java/hu/bme/mit/trainbenchmark/benchmark/emfincquery/driver/EMFIncQueryBaseDriver.java
+++ hu.bme.mit.trainbenchmark.benchmark.emfincquery/src/main/java/hu/bme/mit/trainbenchmark/benchmark/emfincquery/driver/EMFIncQueryBaseDriver.java
@@ -1,12 +1,23 @@
 package hu.bme.mit.trainbenchmark.benchmark.emfincquery.driver;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.incquery.runtime.api.AdvancedIncQueryEngine;
 import org.eclipse.incquery.runtime.api.impl.BasePatternMatch;
+import org.eclipse.incquery.runtime.base.api.NavigationHelper;
+import org.eclipse.incquery.runtime.emf.EMFScope;
+
+import com.google.common.collect.Sets;
 
 import hu.bme.mit.trainbenchmark.benchmark.emfincquery.checker.EMFIncQueryChecker;
 import hu.bme.mit.trainbenchmark.emf.EMFDriver;
+import hu.bme.mit.trainbenchmark.railway.RailwayElement;
+import hu.bme.mit.trainbenchmark.railway.RailwayPackage;
 
 public abstract class EMFIncQueryBaseDriver<M extends BasePatternMatch> extends EMFDriver {
 
@@ -15,6 +26,21 @@ public abstract class EMFIncQueryBaseDriver<M extends BasePatternMatch> extends 
 
 	public void registerChecker(final EMFIncQueryChecker<M> checker) throws IOException {
 		this.checker = checker;
+	}
+
+	@Override
+	public List<RailwayElement> collectVertices(final String type) throws Exception {
+		final EClass clazz = (EClass) RailwayPackage.eINSTANCE.getEClassifier(type);
+
+		final NavigationHelper navigationHelper = EMFScope.extractUnderlyingEMFIndex(engine);
+		navigationHelper.registerEClasses(Sets.newHashSet(clazz));
+		final Set<EObject> instances = navigationHelper.getAllInstances(clazz);
+		final List<RailwayElement> vertices = new ArrayList<>();
+		for (final EObject instance : instances) {
+			vertices.add((RailwayElement) instance);
+		}
+
+		return vertices;
 	}
 
 	public AdvancedIncQueryEngine getEngine() {
