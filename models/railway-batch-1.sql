4d1df0d69b312b6e0aabc373d9561105bcf80ddd
Gabor Szarnyas <szarnyasg@gmail.com>
2015-11-06 20:14:51 +0100

    Bumped MySQL version in instance models

--- models/railway-batch-1.sql
+++ models/railway-batch-1.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.6.25, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.6.27, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: trainbenchmark
 -- ------------------------------------------------------
--- Server version	5.6.25-0ubuntu0.15.04.1
+-- Server version	5.6.27-0ubuntu1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
