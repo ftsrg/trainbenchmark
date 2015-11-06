92a09265f72761fd29ca72401284c232d3905959
Gabor Szarnyas <szarnyasg@gmail.com>
2015-11-06 20:02:31 +0100

    Fixed MONDO-SAM dependency

--- scripts/dep-mondo-sam.sh
+++ scripts/dep-mondo-sam.sh
@@ -3,7 +3,7 @@
 cd "$( cd "$( dirname "$0" )" && pwd )/../.."
 
 if [ ! -d mondo-sam ]; then
-  git clone --branch issue-9 https://github.com/FTSRG/mondo-sam.git
+  git clone --branch v0.1.0 https://github.com/FTSRG/mondo-sam
 fi
 cd mondo-sam
 mvn clean install -DskipTests
