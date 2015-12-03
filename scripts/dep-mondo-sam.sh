daea00a812ac02fe1d05b81787d858132ac3d054
Gabor Szarnyas <szarnyasg@gmail.com>
2015-12-03 23:04:25 +0100

    Fixed MONDO SAM issue

--- scripts/dep-mondo-sam.sh
+++ scripts/dep-mondo-sam.sh
@@ -3,8 +3,10 @@
 cd "$( cd "$( dirname "$0" )" && pwd )/../.."
 
 if [ ! -d mondo-sam ]; then
-  git clone --branch v0.1.0 https://github.com/FTSRG/mondo-sam.git
+  # the --branch option is only support by git 1.7.10+
+  git clone https://github.com/FTSRG/mondo-sam.git
 fi
 cd mondo-sam
+git checkout v0.1.0
 mvn clean install -DskipTests
 cd ..
