a37692c32c938b975d4b2f92f8bd81ae3b1385ba
János Maginecz <wafleeee@gmail.com>
2016-01-02 17:42:13 +0100

    add depth argument to git clone commands

--- scripts/dep-graph.sh
+++ scripts/dep-graph.sh
@@ -3,6 +3,7 @@
 cd "$( cd "$( dirname "$0" )" && pwd )/../.."
 
 if [ ! -d neo4j-shell-tools ]; then
+  #do not add depth here, the branch requires deeper clone
   git clone https://github.com/jexp/neo4j-shell-tools
 fi
 cd neo4j-shell-tools
