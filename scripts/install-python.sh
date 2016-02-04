b083fb28214ee3c133a5ce5123baab945fdab431
Gabor Szarnyas <szarnyasg@gmail.com>
2016-02-04 16:42:53 +0100

    Pip

--- scripts/install-python.sh
+++ scripts/install-python.sh
@@ -6,9 +6,7 @@ sudo apt-get -y update
 sudo apt-get install -y python3 python3-setuptools python3-dev
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
-sudo pip3 install -r ../config/requirements.txt
-pip3 --version
-pip3 freeze
 sudo pip3 install --upgrade pip
+sudo pip3 install -r ../config/requirements.txt
 pip3 --version
 pip3 freeze
