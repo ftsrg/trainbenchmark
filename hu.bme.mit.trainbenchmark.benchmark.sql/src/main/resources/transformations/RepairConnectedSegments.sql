90cb9c2ae6557240e8358241865e7ad19936ae28
Gabor Szarnyas <szarnyasg@gmail.com>
2016-02-04 10:18:52 +0100

    Repair transformations fixed

--- hu.bme.mit.trainbenchmark.benchmark.sql/src/main/resources/transformations/RepairConnectedSegments.sql
+++ hu.bme.mit.trainbenchmark.benchmark.sql/src/main/resources/transformations/RepairConnectedSegments.sql
@@ -4,11 +4,16 @@ SET @segment3 := ?;
 
 -- delete (segment2) node as segment
 DELETE FROM Segment WHERE id = @segment2;
--- delete (segment2) node as TrackELement and sensor edge
+-- delete (segment2) node as TrackElement and sensor edge
 DELETE FROM TrackElement WHERE id = @segment2;
+
 -- delete (segment1)-[:connectsTo]->(segment2) edge
-DELETE FROM connectsTo WHERE TrackElement1 = @segment1 AND TrackElement2 = @segment2;
+DELETE FROM connectsTo WHERE TrackElement1_id = @segment1 AND TrackElement2_id = @segment2;
 -- delete (segment2)-[:connectsTo]->(segment3) edge
-DELETE FROM connectsTo WHERE TrackElement1 = @segment2  AND TrackElement2 = @segment3;
+DELETE FROM connectsTo WHERE TrackElement1_id = @segment2  AND TrackElement2_id = @segment3;
+
+-- delete (segment2)-[:monitoredBy]->(sensor) edge
+DELETE FROM monitoredBy WHERE TrackElement_id = @segment2  AND Sensor_id = @sensor;
+
 -- insert (segment1)-[:connectsTo]->(segment3) edge
-INSERT INTO definedBy (Route_id, Sensor_id) VALUES (@segment1, @segment3);
+INSERT INTO connectsTo VALUES (@segment1, @segment3);
