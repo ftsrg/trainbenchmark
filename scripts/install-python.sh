9991c658109f39b405bf349c93958cad0c1688a4
wafle <wafleeee@gmail.com>
2016-02-04 17:55:30 +0100

    remove sudo from python calls

--- scripts/install-python.sh
+++ scripts/install-python.sh
@@ -5,6 +5,6 @@ cd "$( cd "$( dirname "$0" )" && pwd )"
 sudo apt-get -y update
 sudo apt-get install -y python3 python3-setuptools python3-dev
 wget https://bootstrap.pypa.io/get-pip.py
-sudo python3 get-pip.py
-sudo pip3 install --upgrade pip
-sudo pip3 install -r ../config/requirements.txt
+python3 get-pip.py
+pip3 install --upgrade pip
+pip3 install -r ../config/requirements.txt
