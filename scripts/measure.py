a815bac731f872f52055c34292f704455c17dbf7
Gabor Szarnyas <szarnyasg@gmail.com>
2016-02-12 01:16:31 +0100

    Added binary search-like operation to get the actual memory consumption

--- scripts/measure.py
+++ scripts/measure.py
@@ -31,43 +31,53 @@ def measure_tools(java_opts: List[str], timeout: int, runs: int, scenarios: List
 
             for query_mix in query_mixes:
                 for tool_option_set in tool_option_sets:
-                    measure_tool(java_opts, timeout, target, scenario_name, runs, sizes, query_mix, transformation_arguments, tool_option_set)
+                    for size in sizes:
+                        measure_tool(java_opts, timeout, target, scenario_name, runs, size, query_mix, transformation_arguments, tool_option_set)
 
             util.set_working_directory("..")
     send_mail.send_mail(email)
 
 
-def measure_tool(java_opts: List[str], timeout: int, target: str, scenario_name: str, runs: int, sizes: List[int], query_mix: List[str], transformation_arguments: List, tool_option_set: List):
-    for size in sizes:
-        # remove all files in the temporary results directory
-        prev_files = glob.glob('../results/json/*')
-        for f in prev_files:
-            os.remove(f)
-
+def measure_tool(java_opts: List[str], timeout: int, target: str, scenario_name: str, runs: int, size: int, query_mix: List[str], transformation_arguments: List, tool_option_set: List):
         options = util.get_command_line_options(tool_option_set)
+        level = 0
+        max_level = 8
+        initial_memory = 12800
+        max_memory = initial_memory
+        memory_quantum = max_memory
 
-        cmd = util.flatten(["java",
-               java_opts,
-               "-jar", target,
-               "-runs", str(runs),
-               "-scenario", scenario_name,
-               "-querymix", query_mix.split(" "),
-               "-size", str(size),
-               transformation_arguments,
-               options])
-        print(util.highlight(" ".join(cmd), True, True))
+        while (level < max_level and max_memory <= initial_memory):
+            level += 1
+            memory_quantum //= 2
+            print("Level " + str(level) + ". Testing with maximum memory of " + str(max_memory) + " MB".)
 
-        try:
-            subprocess.check_call(cmd, timeout=timeout)
-        except subprocess.TimeoutExpired:
-            print(util.highlight("Timeout, skipping larger sizes for this tool, scenario and query mix.", True, True))
-            break
-        except subprocess.CalledProcessError:
-            print(util.highlight("An error occured, skipping larger sizes for this tool, scenario and query mix.", True, True))
-            break
+            cmd = util.flatten(["java",
+                   java_opts,
+                   "-Xmx" + str(max_memory) + "M",
+                   "-jar", target,
+                   "-runs", str(runs),
+                   "-scenario", scenario_name,
+                   "-queryMix", query_mix.split(" "),
+                   "-maxMemory", str(max_memory),
+                   "-size", str(size),
+                   transformation_arguments,
+                   options])
 
-        # if the runs were successful, move all files to the results
-        result_files = glob.glob('../results/json/*')
-        for f in result_files:
-            name = os.path.basename(f)
-            os.rename(f, '../results/completed/' + name)
+            print(util.highlight(" ".join(cmd), True, True))
+            if test(cmd, timeout):
+                print("Execution finished, testing with less memory.")
+                max_memory -= memory_quantum
+            else:
+                print("Execution failed, testing with more memory.")
+                max_memory += memory_quantum
+
+def test(cmd, timeout):
+    try:
+        subprocess.check_call(cmd, timeout=timeout)
+    except subprocess.TimeoutExpired:
+        print(util.highlight("Timeout, skipping larger sizes for this tool, scenario and query mix.", True, True))
+        return False
+    except subprocess.CalledProcessError:
+        print(util.highlight("An error occured, skipping larger sizes for this tool, scenario and query mix.", True, True))
+        return False
+    return True
