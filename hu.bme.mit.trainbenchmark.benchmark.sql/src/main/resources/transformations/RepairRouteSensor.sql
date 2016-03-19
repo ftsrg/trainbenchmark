0d35824656c4cf27b937be4ad8a5880979db78ba
Gabor Szarnyas <szarnyasg@gmail.com>
2016-03-19 11:55:16 +0100

    Added install script, fixed another error

--- hu.bme.mit.trainbenchmark.benchmark.sql/src/main/resources/transformations/RepairRouteSensor.sql
+++ hu.bme.mit.trainbenchmark.benchmark.sql/src/main/resources/transformations/RepairRouteSensor.sql
@@ -1,7 +1,4 @@
-SET @route := ?;
-SET @sensor := ?;
-
 -- the "Sensor.route" attribute is the inverse of the "Route.gathers" edge
 UPDATE Sensor
-SET route = @route
-WHERE id = @sensor;
+SET route = ?
+WHERE id = ?;
