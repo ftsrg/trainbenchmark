a571512999fce23da0db87977132449d04c13c4e
Gabor Szarnyas <szarnyasg@gmail.com>
2016-04-24 19:46:22 +0200

    Some simplifications in the Neo4j code, added foreach loops instead of ifs

--- hu.bme.mit.trainbenchmark.benchmark.tinkergraph/src/main/java/hu/bme/mit/trainbenchmark/benchmark/tinkergraph/checkers/core/TinkerGraphConnectedSegmentsChecker.java
+++ hu.bme.mit.trainbenchmark.benchmark.tinkergraph/src/main/java/hu/bme/mit/trainbenchmark/benchmark/tinkergraph/checkers/core/TinkerGraphConnectedSegmentsChecker.java
@@ -14,10 +14,17 @@ package hu.bme.mit.trainbenchmark.benchmark.tinkergraph.checkers.core;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.tinkerpop.gremlin.structure.Direction;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
 
 import hu.bme.mit.trainbenchmark.benchmark.tinkergraph.checkers.TinkerGraphChecker;
 import hu.bme.mit.trainbenchmark.benchmark.tinkergraph.driver.TinkerGraphDriver;
 import hu.bme.mit.trainbenchmark.benchmark.tinkergraph.matches.TinkerGraphConnectedSegmentsMatch;
+import hu.bme.mit.trainbenchmark.constants.ModelConstants;
+import hu.bme.mit.trainbenchmark.constants.QueryConstants;
 
 public class TinkerGraphConnectedSegmentsChecker extends TinkerGraphChecker<TinkerGraphConnectedSegmentsMatch> {
 
@@ -29,126 +36,89 @@ public class TinkerGraphConnectedSegmentsChecker extends TinkerGraphChecker<Tink
 	public Collection<TinkerGraphConnectedSegmentsMatch> check() throws IOException {
 		final Collection<TinkerGraphConnectedSegmentsMatch> matches = new ArrayList<>();
 
-//		final List<? extends Vertex> sensors = TinkerHelper.queryVertexIndex(graph, TYPE, SENSOR);
-		
-//		final GraphDatabaseService graphDb = driver.getGraphDb();
-//		try (Transaction tx = graphDb.beginTx()) {
-//
-//			// (sensor:Sensor)
-//			final ResourceIterator<Node> sensors = graphDb.findNodes(Neo4jConstants.labelSensor);
-//			while (sensors.hasNext()) {
-//				final Node sensor = sensors.next();
-//				
-//				// (sensor:Sensor)<-[:sensor]-(segment1:Segment)
-//				final Iterator<Relationship> sensorEdges1 = sensor.getRelationships(Direction.INCOMING,
-//						Neo4jConstants.relationshipTypeMonitoredBy).iterator();			
-//				while (sensorEdges1.hasNext()) {
-//					final Node segment1 = sensorEdges1.next().getStartNode();
-//					if (!segment1.hasLabel(Neo4jConstants.labelSegment)) {
-//						continue;
-//					}
-//					
-//					// (segment1:Segment)-[:connectsTo]->(segment2:Segment)
-//					final Iterator<Relationship> connectsTo1s = segment1.getRelationships(Direction.OUTGOING,
-//							Neo4jConstants.relationshipTypeConnectsTo).iterator();
-//					if (!connectsTo1s.hasNext()) {
-//						continue;
-//					}
-//
-//					final Node segment2 = connectsTo1s.next().getEndNode();
-//					if (!segment2.hasLabel(Neo4jConstants.labelSegment)) {
-//						continue;
-//					}
-//
-//					// (segment2:Segment)-[:sensor]->(sensor:Sensor)
-//					if (!isConnected(segment2, sensor, Neo4jConstants.relationshipTypeMonitoredBy)) {
-//						continue;
-//					}
-//					
-//					// (segment2:Segment)-[:connectsTo]->(segment3:Segment)
-//					final Iterator<Relationship> connectsTo2s = segment2.getRelationships(Direction.OUTGOING,
-//							Neo4jConstants.relationshipTypeConnectsTo).iterator();
-//					if (!connectsTo2s.hasNext()) {
-//						continue;
-//					}
-//
-//					final Node segment3 = connectsTo2s.next().getEndNode();
-//					if (!segment3.hasLabel(Neo4jConstants.labelSegment)) {
-//						continue;
-//					}
-//					
-//					// (segment3:Segment)-[:sensor]->(sensor:Sensor)
-//					if (!isConnected(segment3, sensor, Neo4jConstants.relationshipTypeMonitoredBy)) {
-//						continue;
-//					}
-//
-//					// (segment3:Segment)-[:connectsTo]->(segment4:Segment)
-//					final Iterator<Relationship> connectsTo3s = segment3.getRelationships(Direction.OUTGOING,
-//							Neo4jConstants.relationshipTypeConnectsTo).iterator();
-//					if (!connectsTo3s.hasNext()) {
-//						continue;
-//					}
-//
-//					final Node segment4 = connectsTo3s.next().getEndNode();
-//					if (!segment4.hasLabel(Neo4jConstants.labelSegment)) {
-//						continue;
-//					}
-//					
-//					// (segment4:Segment)-[:sensor]->(sensor:Sensor)
-//					if (!isConnected(segment4, sensor, Neo4jConstants.relationshipTypeMonitoredBy)) {
-//						continue;
-//					}				
-//
-//					// (segment4:Segment)-[:connectsTo]->(segment5:Segment)
-//					final Iterator<Relationship> connectsTo4s = segment4.getRelationships(Direction.OUTGOING,
-//							Neo4jConstants.relationshipTypeConnectsTo).iterator();
-//					if (!connectsTo4s.hasNext()) {
-//						continue;
-//					}
-//
-//					final Node segment5 = connectsTo4s.next().getEndNode();
-//					if (!segment5.hasLabel(Neo4jConstants.labelSegment)) {
-//						continue;
-//					}
-//					
-//					// (segment5:Segment)-[:sensor]->(sensor:Sensor)
-//					if (!isConnected(segment5, sensor, Neo4jConstants.relationshipTypeMonitoredBy)) {
-//						continue;
-//					}
-//
-//					// (segment5:Segment)-[:connectsTo]->(segment6:Segment)
-//					final Iterator<Relationship> connectsTo5s = segment5.getRelationships(Direction.OUTGOING,
-//							Neo4jConstants.relationshipTypeConnectsTo).iterator();
-//					if (!connectsTo5s.hasNext()) {
-//						continue;
-//					}
-//
-//					final Node segment6 = connectsTo5s.next().getEndNode();
-//					if (!segment6.hasLabel(Neo4jConstants.labelSegment)) {
-//						continue;
-//					}
-//
-//					// (segment6:Segment)-[:sensor]->(sensor:Sensor)
-//					if (!isConnected(segment6, sensor, Neo4jConstants.relationshipTypeMonitoredBy)) {
-//						continue;
-//					}
-//					
-//					final Map<String, Object> match = new HashMap<>();
-//					match.put(VAR_SENSOR, sensor);
-//					match.put(VAR_SEGMENT1, segment1);
-//					match.put(VAR_SEGMENT2, segment2);
-//					match.put(VAR_SEGMENT3, segment3);
-//					match.put(VAR_SEGMENT4, segment4);
-//					match.put(VAR_SEGMENT5, segment5);
-//					match.put(VAR_SEGMENT6, segment6);
-//					final Neo4jConnectedSegmentsMatch csm = new Neo4jConnectedSegmentsMatch(match);
-//					matches.add(csm);
-//				}
-//			}
-//		}
+		final Collection<Vertex> sensors = driver.collectVertices(ModelConstants.SENSOR);
+		for (final Vertex sensor : sensors) {
+			// (sensor:Sensor)<-[:sensor]-(segment1:Segment)
+			final Iterable<Vertex> segments1 = () -> sensor.vertices(Direction.IN, ModelConstants.MONITORED_BY);
+
+			for (final Vertex segment1 : segments1) {
+				if (!segment1.label().equals(ModelConstants.SEGMENT)) {
+					continue;
+				}
+
+				// (segment2:Segment)-[:sensor]->(sensor:Sensor)
+				final Iterable<Vertex> segment2s = () -> segment1.vertices(Direction.OUT, ModelConstants.CONNECTS_TO);
+				for (final Vertex segment2 : segment2s) {
+					if (!segment2.label().equals(ModelConstants.SEGMENT) || !isConnected(segment2, sensor, ModelConstants.MONITORED_BY)) {
+						continue;
+					}
+
+					// (segment2:Segment)-[:connectsTo]->(segment3:Segment)
+					// (segment3:Segment)-[:sensor]->(sensor:Sensor)
+					final Iterable<Vertex> segment3s = () -> segment2.vertices(Direction.OUT, ModelConstants.CONNECTS_TO);
+					for (final Vertex segment3 : segment3s) {
+						if (!segment3.label().equals(ModelConstants.SEGMENT)
+								|| !isConnected(segment3, sensor, ModelConstants.MONITORED_BY)) {
+							continue;
+						}
+
+						// (segment3:Segment)-[:connectsTo]->(segment4:Segment)
+						// (segment4:Segment)-[:sensor]->(sensor:Sensor)
+						final Iterable<Vertex> segment4s = () -> segment3.vertices(Direction.OUT, ModelConstants.CONNECTS_TO);
+						for (final Vertex segment4 : segment4s) {
+							if (!segment4.label().equals(ModelConstants.SEGMENT)
+									|| !isConnected(segment4, sensor, ModelConstants.MONITORED_BY)) {
+								continue;
+							}
+
+							// (segment4:Segment)-[:connectsTo]->(segment5:Segment)
+							// (segment5:Segment)-[:sensor]->(sensor:Sensor)
+							final Iterable<Vertex> segment5s = () -> segment4.vertices(Direction.OUT, ModelConstants.CONNECTS_TO);
+							for (final Vertex segment5 : segment5s) {
+								if (!segment5.label().equals(ModelConstants.SEGMENT)
+										|| !isConnected(segment5, sensor, ModelConstants.MONITORED_BY)) {
+									continue;
+								}
+
+								// (segment5:Segment)-[:connectsTo]->(segment6:Segment)
+								// (segment6:Segment)-[:sensor]->(sensor:Sensor)
+								final Iterable<Vertex> segment6s = () -> segment5.vertices(Direction.OUT, ModelConstants.CONNECTS_TO);
+								for (final Vertex segment6 : segment6s) {
+									if (!segment6.label().equals(ModelConstants.SEGMENT)
+											|| !isConnected(segment6, sensor, ModelConstants.MONITORED_BY)) {
+										continue;
+									}
+
+									final Map<String, Object> match = new HashMap<>();
+									match.put(QueryConstants.VAR_SENSOR, sensor);
+									match.put(QueryConstants.VAR_SEGMENT1, segment1);
+									match.put(QueryConstants.VAR_SEGMENT2, segment6);
+									match.put(QueryConstants.VAR_SEGMENT3, segment3);
+									match.put(QueryConstants.VAR_SEGMENT4, segment4);
+									match.put(QueryConstants.VAR_SEGMENT5, segment5);
+									match.put(QueryConstants.VAR_SEGMENT6, segment6);
+									final TinkerGraphConnectedSegmentsMatch csm = new TinkerGraphConnectedSegmentsMatch(match);
+									matches.add(csm);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 
 		return matches;
+
 	}
 
+	private boolean isConnected(final Vertex source, final Vertex target, final String type) {
+		final Iterable<Vertex> vertices = () -> source.vertices(Direction.OUT, type);
+		for (final Vertex vertex : vertices) {
+			if (vertex.equals(target)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 }
