a0ea59aa1477f0262d419d635a4b4ec00f079ecc
Gabor Szarnyas <szarnyasg@gmail.com>
2016-06-01 20:24:47 +0200

    Fixed build

--- hu.bme.mit.trainbenchmark.benchmark.mysql/src/main/java/hu/bme/mit/trainbenchmark/benchmark/mysql/MySQLBenchmarkCase.java
+++ hu.bme.mit.trainbenchmark.benchmark.mysql/src/main/java/hu/bme/mit/trainbenchmark/benchmark/mysql/MySQLBenchmarkCase.java
@@ -43,7 +43,7 @@ public class MySQLBenchmarkCase extends SQLBenchmarkCase<BenchmarkConfig, MySQLD
 	}
 
 	@Override
-	public Comparator<?> createMatchComparator() {
+	public Comparator<?> getMatchComparator() {
 		return new LongMatchComparator();
 	}
 
