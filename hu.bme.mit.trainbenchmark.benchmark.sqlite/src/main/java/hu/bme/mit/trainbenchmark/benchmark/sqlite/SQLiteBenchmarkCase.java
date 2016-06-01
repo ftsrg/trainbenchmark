512a52f30b26b29dc1d4d2fbe83651caeb529444
Gabor Szarnyas <szarnyasg@gmail.com>
2016-06-01 18:30:18 +0200

    Fixed method name

--- hu.bme.mit.trainbenchmark.benchmark.sqlite/src/main/java/hu/bme/mit/trainbenchmark/benchmark/sqlite/SQLiteBenchmarkCase.java
+++ hu.bme.mit.trainbenchmark.benchmark.sqlite/src/main/java/hu/bme/mit/trainbenchmark/benchmark/sqlite/SQLiteBenchmarkCase.java
@@ -43,7 +43,7 @@ public class SQLiteBenchmarkCase extends SQLBenchmarkCase<BenchmarkConfig, SQLit
 	}
 
 	@Override
-	public Comparator<?> createMatchComparator() {
+	public Comparator<?> getMatchComparator() {
 		return new LongMatchComparator();
 	}
 
