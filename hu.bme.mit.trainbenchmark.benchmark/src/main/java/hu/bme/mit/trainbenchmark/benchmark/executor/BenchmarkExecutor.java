0dd98dab44ec1838ba1010aea3fd18a3524ba598
Gabor Szarnyas <szarnyasg@gmail.com>
2016-06-05 15:21:02 +0200

    Started to add phases

--- hu.bme.mit.trainbenchmark.benchmark/src/main/java/hu/bme/mit/trainbenchmark/benchmark/executor/BenchmarkExecutor.java
+++ hu.bme.mit.trainbenchmark.benchmark/src/main/java/hu/bme/mit/trainbenchmark/benchmark/executor/BenchmarkExecutor.java
@@ -1,6 +1,8 @@
 package hu.bme.mit.trainbenchmark.benchmark.executor;
 
+import java.util.Collection;
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.Random;
 
 import hu.bme.mit.trainbenchmark.benchmark.driver.Driver;
@@ -14,23 +16,44 @@ public class BenchmarkExecutor<TDriver extends Driver<?>, TPatternMatch> {
 	protected final TDriver driver;
 	protected final ModelOperationFactory<TPatternMatch, TDriver> factory;
 	protected final Comparator<TPatternMatch> comparator;
+	protected Collection<QueryShuffleTransformation<? extends TPatternMatch, TDriver>> qsts = new LinkedList<>();
 
 	public BenchmarkExecutor(final TDriver driver, final ModelOperationFactory<TPatternMatch, TDriver> factory,
-			Comparator<TPatternMatch> comparator) {
+			final Comparator<TPatternMatch> comparator) {
 		this.driver = driver;
 		this.factory = factory;
 		this.comparator = comparator;
 	}
 
-	public void executeGeneric(final String modelPath, final RailwayOperation railwayOperation) throws Exception {
-		driver.read(modelPath);
-
-		final ModelOperation<? extends TPatternMatch, TDriver> operation = factory.createOperation(railwayOperation, driver);
-		final QueryShuffleTransformation<? extends TPatternMatch, TDriver> qst = QueryShuffleTransformation.of(operation, comparator,
-				random);
-
-		qst.evaluateQuery();
-		qst.shuffle(10);
-		qst.transform();
+	public void read() throws Exception {
+		driver.read("../models/railway-repair-1.xmi");
 	}
+
+	public void initializeOperations(final Collection<RailwayOperation> railwayOperations) throws Exception {
+		for (final RailwayOperation railwayOperation : railwayOperations) {
+			final ModelOperation<? extends TPatternMatch, TDriver> operation = factory.createOperation(railwayOperation, driver);
+			final QueryShuffleTransformation<? extends TPatternMatch, TDriver> qst = QueryShuffleTransformation.of(operation, comparator, random);
+			qsts.add(qst);
+		}
+	}
+
+	public void query() throws Exception {
+		for (final QueryShuffleTransformation<? extends TPatternMatch, TDriver> qst : qsts) {
+			final Collection<?> matches = qst.evaluateQuery();
+			System.out.println(matches.size());
+		}
+	}
+
+	public void shuffle() {
+		for (final QueryShuffleTransformation<? extends TPatternMatch, TDriver> qst : qsts) {
+			qst.shuffle(10);
+		}
+	}
+
+	public void transform() throws Exception {
+		for (final QueryShuffleTransformation<? extends TPatternMatch, TDriver> qst : qsts) {
+			qst.transform();
+		}
+	}
+
 }
