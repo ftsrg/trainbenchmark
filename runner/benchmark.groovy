c4ae5874b2bd5603ad038088271e80617a63e96a
Gabor Szarnyas <szarnyasg@gmail.com>
2016-06-07 00:24:47 +0200

    Added Groovy script to propert Gradle build, dropped Grapes

--- runner/benchmark.groovy
+++ runner/benchmark.groovy
@@ -1,19 +1,10 @@
-@Grapes([
-	@Grab(group='hu.bme.mit.trainbenchmark', module='hu.bme.mit.trainbenchmark.benchmark', version='1.0.0-SNAPSHOT', changing=true),
-	@Grab(group='com.google.guava', module='guava', version='19.0'),
-	@Grab(group='com.esotericsoftware', module='kryo', version='3.0.3'),
-	@Grab(group='org.ow2.asm', module='asm', version='5.1')
-])
 
-import com.google.common.collect.ImmutableList
-import hu.bme.mit.trainbenchmark.benchmark.config.BenchmarkConfig
 import static hu.bme.mit.trainbenchmark.constants.RailwayOperation.*
+import hu.bme.mit.trainbenchmark.benchmark.config.BenchmarkConfig
+
 import com.esotericsoftware.kryo.Kryo
 import com.esotericsoftware.kryo.io.Input
 import com.esotericsoftware.kryo.io.Output
-
-import java.io.FileInputStream
-import java.io.FileOutputStream
 
 def config = new BenchmarkConfig()
 config.modelPath = "../models/railway-repair-1.xmi"
