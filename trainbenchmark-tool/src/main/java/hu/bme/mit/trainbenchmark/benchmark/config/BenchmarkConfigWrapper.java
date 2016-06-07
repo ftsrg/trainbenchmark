d4d7db27a4c300988553598802191e04c106937c
Gabor Szarnyas <szarnyasg@gmail.com>
2016-06-07 17:24:45 +0200

    Fixed Runner script

--- trainbenchmark-tool/src/main/java/hu/bme/mit/trainbenchmark/benchmark/config/BenchmarkConfigWrapper.java
+++ trainbenchmark-tool/src/main/java/hu/bme/mit/trainbenchmark/benchmark/config/BenchmarkConfigWrapper.java
@@ -1,8 +1,18 @@
 package hu.bme.mit.trainbenchmark.benchmark.config;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
 public class BenchmarkConfigWrapper {
 
 	protected BenchmarkConfig benchmarkConfig;
+	
+	protected BenchmarkConfigWrapper() {}
 	
 	public BenchmarkConfigWrapper(final BenchmarkConfig benchmarkConfig) {
 		this.benchmarkConfig = benchmarkConfig;
@@ -16,4 +26,19 @@ public class BenchmarkConfigWrapper {
 		return "";
 	}
 
+	public void saveToFile(final String path) throws FileNotFoundException {
+		final Kryo kryo = new Kryo();
+		try (final Output output = new Output(new FileOutputStream(path))) {
+			kryo.writeObject(output, this);
+		}
+	}
+
+	public static BenchmarkConfigWrapper fromFile(final String path) throws FileNotFoundException {
+		final Kryo kryo = new Kryo();
+		try (final Input input = new Input(new FileInputStream(path))) {
+			final BenchmarkConfigWrapper benchmarkConfig = kryo.readObject(input, BenchmarkConfigWrapper.class);
+			return benchmarkConfig;
+		}
+	}
+	
 }
