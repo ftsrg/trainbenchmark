4b6030d594c53664f08af76234d7895ef8d6483e
Gabor Szarnyas <szarnyasg@gmail.com>
2016-06-08 20:37:58 +0200

    Groovy

---
+++ trainbenchmark-runner/src/main/groovy/hu/bme/mit/trainbenchmark/runner/BenchmarkRunner.groovy
@@ -0,0 +1,10 @@
+package hu.bme.mit.trainbenchmark.runner
+
+
+class BenchmarkRunner {
+
+	public static void runBenchmark(BenchmarkConfigWrapper bcw) {
+		
+	} 
+	
+}
---
+++ trainbenchmark-runner/src/main/java/hu/bme/mit/trainbenchmark/runner/Main.java
@@ -0,0 +1,8 @@
+package hu.bme.mit.trainbenchmark.runner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		
+	}
+}
