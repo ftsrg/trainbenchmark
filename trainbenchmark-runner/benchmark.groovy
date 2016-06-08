154f2dd3eb63f401b587e167845c0b9d27179f83
Gabor Szarnyas <szarnyasg@gmail.com>
2016-06-08 21:03:15 +0200

    Moved most of benchmark runner logic to good ol' Java

--- trainbenchmark-runner/benchmark.groovy
+++ trainbenchmark-runner/benchmark.groovy
@@ -1,7 +1,7 @@
 import static hu.bme.mit.trainbenchmark.constants.RailwayOperation.*
-
-import hu.bme.mit.trainbenchmark.benchmark.config.BenchmarkConfigWrapper
 import hu.bme.mit.trainbenchmark.benchmark.config.BenchmarkConfig
+import hu.bme.mit.trainbenchmark.benchmark.config.BenchmarkConfigWrapper
+import hu.bme.mit.trainbenchmark.runner.BenchmarkRunner
 
 def modelPath = "../models/railway-repair-1.xmi"
 def railwayOperations = [
@@ -10,28 +10,6 @@ def railwayOperations = [
 	SWITCHMONITORED,
 ]
 final BenchmarkConfig bc = new BenchmarkConfig(1, 5, "EMF API", modelPath, railwayOperations)
+
 final BenchmarkConfigWrapper bcw = new BenchmarkConfigWrapper(bc)
-//
-//BenchmarkRunner.runBenchmark(bcw)
-//
-//def configFile = File.createTempFile("trainbenchmark-", ".conf")
-//def configPath = configFile.absolutePath;
-//bcw.saveToFile(configPath)
-//BenchmarkConfigWrapper.fromFile(configPath)
-//
-//def toolName = "emfapi"
-//def projectName = "trainbenchmark-tool-${toolName}"
-//
-//def script = "../${projectName}/build/install/${projectName}/bin/${projectName} ${configPath}"
-//
-//def sout = new StringBuilder()
-//def serr = new StringBuilder()
-//
-//println(script)
-//def proc = script.execute()
-//proc.consumeProcessOutput(sout, serr)
-//proc.waitForOrKill(1000)
-//println "out> $sout"
-//println "err> $serr"
-//
-//configFile.deleteOnExit()
+BenchmarkRunner.runBenchmark(bcw)
--- trainbenchmark-runner/src/main/java/hu/bme/mit/trainbenchmark/runner/Main.java
+++
@@ -1,8 +0,0 @@
-package hu.bme.mit.trainbenchmark.runner;
-
-public class Main {
-	public static void main(String[] args) {
-		
-		
-	}
-}
