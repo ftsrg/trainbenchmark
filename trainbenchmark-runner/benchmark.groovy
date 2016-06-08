e33b2ee9589a1806cdde446e442e40eef99d7a1a
Gabor Szarnyas <szarnyasg@gmail.com>
2016-06-08 20:37:58 +0200

    Groovy

--- trainbenchmark-runner/benchmark.groovy
+++ trainbenchmark-runner/benchmark.groovy
@@ -11,25 +11,27 @@ def railwayOperations = [
 ]
 final BenchmarkConfig bc = new BenchmarkConfig(1, 5, "EMF API", modelPath, railwayOperations)
 final BenchmarkConfigWrapper bcw = new BenchmarkConfigWrapper(bc)
-
-def configFile = File.createTempFile("trainbenchmark-", ".conf")
-def configPath = configFile.absolutePath;
-bcw.saveToFile(configPath)
-BenchmarkConfigWrapper.fromFile(configPath)
-
-def toolName = "emfapi"
-def projectName = "trainbenchmark-tool-${toolName}"
-
-def script = "../${projectName}/build/install/${projectName}/bin/${projectName} ${configPath}"
-
-def sout = new StringBuilder()
-def serr = new StringBuilder()
-
-println(script)
-def proc = script.execute()
-proc.consumeProcessOutput(sout, serr)
-proc.waitForOrKill(1000)
-println "out> $sout"
-println "err> $serr"
-
-configFile.deleteOnExit()
+//
+//BenchmarkRunner.runBenchmark(bcw)
+//
+//def configFile = File.createTempFile("trainbenchmark-", ".conf")
+//def configPath = configFile.absolutePath;
+//bcw.saveToFile(configPath)
+//BenchmarkConfigWrapper.fromFile(configPath)
+//
+//def toolName = "emfapi"
+//def projectName = "trainbenchmark-tool-${toolName}"
+//
+//def script = "../${projectName}/build/install/${projectName}/bin/${projectName} ${configPath}"
+//
+//def sout = new StringBuilder()
+//def serr = new StringBuilder()
+//
+//println(script)
+//def proc = script.execute()
+//proc.consumeProcessOutput(sout, serr)
+//proc.waitForOrKill(1000)
+//println "out> $sout"
+//println "err> $serr"
+//
+//configFile.deleteOnExit()
