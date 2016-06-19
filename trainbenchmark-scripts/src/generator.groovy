ea89ab18621189ffde180c183095b4aca9fb0ee4
Gabor Szarnyas <szarnyasg@gmail.com>
2016-06-19 12:22:16 +0200

    Wrapped script to classes to allow quick lookup in Eclipse

--- trainbenchmark-scripts/src/generator.groovy
+++ trainbenchmark-scripts/src/generator.groovy
@@ -7,51 +7,56 @@ import hu.bme.mit.trainbenchmark.generator.rdf.config.RdfGeneratorConfigWrapper
 import hu.bme.mit.trainbenchmark.generator.runner.GeneratorRunner
 import hu.bme.mit.trainbenchmark.rdf.RdfFormat
 
+class GeneratorScript extends Script {
 
-def xms = "2G"
-def xmx = "4G"
-def minSize = 1
-def maxSize = 2
+	def run() {
+		def xms = "2G"
+		def xmx = "4G"
+		def minSize = 1
+		def maxSize = 2
 
-def scenarios = [
-	Scenario.BATCH,
-	Scenario.INJECT,
-	Scenario.REPAIR,
-]
+		def scenarios = [
+			Scenario.BATCH,
+			Scenario.INJECT,
+			Scenario.REPAIR,
+		]
 
-def generate(int xms, int xmx, Scenario scenario, int size) {
-	def gc = new GeneratorConfig(xms, xmx, scenario, size)
-
-	// EMF
-	def egcw = new GeneratorConfigWrapper(gc)
-	GeneratorRunner.run(egcw, "emf")
-
-	// graph / Neo4j
-	def gngcw = new GeneratorConfigWrapper(gc)
-	GeneratorRunner.run(gngcw, "graph-neo4j")
-
-	// graph / TinkerPop
-	def gtgcw = new TinkerPopGraphGeneratorConfigWrapper(gc, GraphFormat.GRAPHML)
-	GeneratorRunner.run(gtgcw, "graph-tinkerpop")
-
-	// RDF
-	def rdfFormats = [RdfFormat.TURTLE]
-	def inferencingOptions = [true, false]
-	for (rdfFormat in rdfFormats) {
-		for (inferencingOption in inferencingOptions) {
-			def rgcw = new RdfGeneratorConfigWrapper(gc, inferencingOption, rdfFormat)
-			GeneratorRunner.run(rgcw, "rdf")
+		for (scenario in scenarios) {
+			for (size = minSize; size <= maxSize; size*=2) {
+				println("Scenario: ${scenario}, size: ${size}")
+				generate(xms, xmx, scenario, size)
+			}
 		}
 	}
 
-	// SQL
-	def sgcw = new GeneratorConfigWrapper(gc)
-	//	GeneratorRunner.run(sgcw, "sql")
-}
+	def generate(int xms, int xmx, Scenario scenario, int size) {
+		def gc = new GeneratorConfig(xms, xmx, scenario, size)
 
-for (scenario in scenarios) {
-	for (size = minSize; size <= maxSize; size*=2) {
-		println("Scenario: ${scenario}, size: ${size}")
-		generate(xms, xmx, scenario, size)
+		// EMF
+		def egcw = new GeneratorConfigWrapper(gc)
+		GeneratorRunner.run(egcw, "emf")
+
+		// graph / Neo4j
+		def gngcw = new GeneratorConfigWrapper(gc)
+		GeneratorRunner.run(gngcw, "graph-neo4j")
+
+		// graph / TinkerPop
+		def gtgcw = new TinkerPopGraphGeneratorConfigWrapper(gc, GraphFormat.GRAPHML)
+		GeneratorRunner.run(gtgcw, "graph-tinkerpop")
+
+		// RDF
+		def rdfFormats = [RdfFormat.TURTLE]
+		def inferencingOptions = [true, false]
+		for (rdfFormat in rdfFormats) {
+			for (inferencingOption in inferencingOptions) {
+				def rgcw = new RdfGeneratorConfigWrapper(gc, inferencingOption, rdfFormat)
+				GeneratorRunner.run(rgcw, "rdf")
+			}
+		}
+
+		// SQL
+		def sgcw = new GeneratorConfigWrapper(gc)
+		//	GeneratorRunner.run(sgcw, "sql")
 	}
+
 }
