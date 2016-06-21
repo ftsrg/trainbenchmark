ebd0723bbbb75e79f2d1cf8dd357c0e8bbb6d3fd
Gabor Szarnyas <szarnyasg@gmail.com>
2016-06-21 09:38:14 +0200

    Added MySQL install

---
+++ scripts/install-mysql-silent.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password '
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password '
+sudo apt-get -y install mysql-server
