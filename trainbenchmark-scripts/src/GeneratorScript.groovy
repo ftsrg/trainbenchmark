154ed52ca73fb43c6a3bef1a3e56ea1193a5c9e5
Gabor Szarnyas <szarnyasg@gmail.com>
2016-06-22 14:54:42 +0200

    Generator fixed

--- trainbenchmark-scripts/src/GeneratorScript.groovy
+++ trainbenchmark-scripts/src/GeneratorScript.groovy
@@ -1,6 +1,8 @@
 import hu.bme.mit.trainbenchmark.constants.Scenario
 import hu.bme.mit.trainbenchmark.generator.config.GeneratorConfig
 import hu.bme.mit.trainbenchmark.generator.config.GeneratorConfigWrapper
+import hu.bme.mit.trainbenchmark.generator.graph.tinkerpop.config.GraphFormat
+import hu.bme.mit.trainbenchmark.generator.graph.tinkerpop.config.TinkerPopGraphGeneratorConfigWrapper
 import hu.bme.mit.trainbenchmark.generator.rdf.config.RdfGeneratorConfigWrapper
 import hu.bme.mit.trainbenchmark.generator.runner.GeneratorRunner
 import hu.bme.mit.trainbenchmark.rdf.RdfFormat
@@ -10,17 +12,17 @@ import hu.bme.mit.trainbenchmark.rdf.RdfFormat
 def generate(String xms, String xmx, Scenario scenario, int size) {
 	def gc = new GeneratorConfig(xms, xmx, scenario, size)
 
-//	// EMF
-//	def egcw = new GeneratorConfigWrapper(gc)
-//	GeneratorRunner.run(egcw, "emf")
-//
-//	// graph / Neo4j
-//	def gngcw = new GeneratorConfigWrapper(gc)
-//	GeneratorRunner.run(gngcw, "graph-neo4j")
-//
-//	// graph / TinkerPop
-//	def gtgcw = new TinkerPopGraphGeneratorConfigWrapper(gc, GraphFormat.GRAPHML)
-//	GeneratorRunner.run(gtgcw, "graph-tinkerpop")
+	// EMF
+	def egcw = new GeneratorConfigWrapper(gc)
+	GeneratorRunner.run(egcw, "emf")
+
+	// graph / Neo4j
+	def gngcw = new GeneratorConfigWrapper(gc)
+	GeneratorRunner.run(gngcw, "graph-neo4j")
+
+	// graph / TinkerPop
+	def gtgcw = new TinkerPopGraphGeneratorConfigWrapper(gc, GraphFormat.GRAPHML)
+	GeneratorRunner.run(gtgcw, "graph-tinkerpop")
 
 	// RDF
 	def rdfFormats = [RdfFormat.TURTLE]
