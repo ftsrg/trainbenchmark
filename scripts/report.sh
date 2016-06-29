9f57f460515a9fdacaf59e04d2a621c36a40908d
Gabor Szarnyas <szarnyasg@gmail.com>
2016-06-29 16:36:57 +0200

    Removed report script

--- scripts/report.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd "$( cd "$( dirname "$0" )" && pwd )"
-
-python3 ../../mondo-sam/reporting/report.py --source ../../trainbenchmark/results/results.csv --output ../../trainbenchmark/diagrams/ --config ../../trainbenchmark/config/reporting.json
