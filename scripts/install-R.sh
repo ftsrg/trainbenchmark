539b8a591b5c45eafb65b68a45c0c6305274aadd
Gabor Szarnyas <szarnyasg@gmail.com>
2016-07-02 18:33:50 +0200

    Fixed install-R.sh script

--- scripts/install-R.sh
+++ scripts/install-R.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # based on https://www.digitalocean.com/community/tutorials/how-to-set-up-r-on-ubuntu-14-04
-# trusty for 14.04, xenial for 16.04
-DISTRIBUTION=trusty
 
-sudo sh -c 'echo "deb http://cran.rstudio.com/bin/linux/ubuntu $DISTRIBUTION/" >> /etc/apt/sources.list'
+# change trusty (14.04) to xenial for 16.04
+sudo sh -c 'echo "deb http://cran.rstudio.com/bin/linux/ubuntu trusty/" >> /etc/apt/sources.list'
 gpg --keyserver keyserver.ubuntu.com --recv-key E084DAB9
 gpg -a --export E084DAB9 | sudo apt-key add -
 
