d17e700f59bfd8f0e35b99594f814fdcacc7d136
Gabor Szarnyas <szarnyasg@gmail.com>
2016-07-05 19:32:42 +0200

    Added 'E' variant w/ Cartesian product

--- trainbenchmark-tool-iqdcore/src/test/java/hu/bme/mit/trainbenchmark/benchmark/iqdcore/test/IQDCoreSemaphoreNeighborTest.java
+++ trainbenchmark-tool-iqdcore/src/test/java/hu/bme/mit/trainbenchmark/benchmark/iqdcore/test/IQDCoreSemaphoreNeighborTest.java
@@ -29,7 +29,7 @@ public class IQDCoreSemaphoreNeighborTest extends SemaphoreNeighborTest {
 
 	@Parameters
 	public static Iterable<? extends Object> data() {
-		return Arrays.asList("D", "A", "B", "C");
+		return Arrays.asList("A", "B", "C", "D", "E");
 	}
 
 	@Parameter
