203ba277be943e0190274ea4c4fcefc8fcece51c
Gabor Szarnyas <szarnyasg@gmail.com>
2016-07-10 12:20:33 +0200

    Updated R script

--- trainbenchmark-reporting/report.R
+++ trainbenchmark-reporting/report.R
@@ -6,7 +6,7 @@ library(ggrepel)
 
 myyaxis = function() {
   # y axis labels
-  longticks = c(F, F, F, T, F, F, F, F, T)
+  longticks = c(T, F, F, T, F, F, F, F, T)
   shortticks = 2:10
   range = -6:4
   
@@ -44,6 +44,8 @@ times.derived = times.wide
 times.derived$Read.and.Check = times.derived$Read + times.derived$Check
 times.derived$Transformation.and.Recheck = times.derived$Transformation + times.derived$Recheck
 
+phases = c("Read", "Check", "Read.and.Check", "Transformation", "Recheck", "Transformation.and.Recheck")
+
 times.aggregated.runs = ddply(
   .data = times.derived,
   .variables = c("Tool", "Workload", "Description", "Model"),
@@ -56,7 +58,7 @@ times.aggregated.runs
 times.plot = melt(
   data = times.aggregated.runs,
   id.vars = c("Tool", "Workload", "Description", "Model", "Run"),
-  measure.vars = c("Read", "Check", "Read.and.Check", "Transformation", "Recheck", "Transformation.and.Recheck"),
+  measure.vars = phases,
   variable.name = "Phase",
   value.name = "Time"
 )
@@ -84,11 +86,24 @@ for (workload in workloads) {
   ybreaks = yaxis$ybreaks
   ylabels = yaxis$ylabels
 
+  maxs <- data.frame(
+    Model = rep(1,6), 
+    Phase = phases,
+    Time = c(20,20,20,0.002,0.002,0.002)
+  )
+  mins <- data.frame(
+    Model = rep(1,6), 
+    Phase = phases,
+    Time = c(0.02,0.02,0.02,0.00001,0.00001,0.00001)
+  )
+  
   p = ggplot(na.omit(df)) +
-    aes(x = as.factor(Model)) +
+    aes(x = as.factor(Model), y = Time) +
     labs(title = workload, x = "Model size\n#Triples", y = "Execution times [s]") +
-    geom_point(aes(y = Time, col = Description, shape = Description), size = 2.0) +
-    geom_line(aes(y = Time, col = Description, group = Description), size = 0.5) +
+    geom_point(aes(col = Description, shape = Description), size = 2.0) +
+    geom_point(data = mins, color = "transparent") +
+    geom_point(data = maxs, color = "transparent") +
+    geom_line(aes(col = Description, group = Description), size = 0.5) +
     scale_x_discrete(breaks = xbreaks, labels = xlabels) +
     scale_y_log10(breaks = ybreaks, labels = ylabels) +
     facet_wrap(~ Phase, ncol = 3, scale = "free_y") +
