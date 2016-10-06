992458510b90c5710351831a329d8f13afe61abb
Gabor Szarnyas <szarnyasg@gmail.com>
2016-10-06 10:19:43 +0200

    Added placeholder project for Epsilon

---
+++ trainbenchmark-tool-epsilon/src/main/java/hu/bme/mit/trainbenchmark/benchmark/emfapi/benchmarkcases/EmfApiQuerySwitchMonitored.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2015, Benedek Izso, Gabor Szarnyas, Istvan Rath and Daniel Varro
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Benedek Izso - initial API and implementation
+ *   Gabor Szarnyas - initial API and implementation
+ *******************************************************************************/
+
+package hu.bme.mit.trainbenchmark.benchmark.emfapi.benchmarkcases;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.emf.common.util.EList;
+
+import hu.bme.mit.trainbenchmark.benchmark.emf.driver.EmfDriver;
+import hu.bme.mit.trainbenchmark.benchmark.emf.matches.EmfSwitchMonitoredMatch;
+import hu.bme.mit.trainbenchmark.benchmark.emf.transformation.query.EmfApiQuery;
+import hu.bme.mit.trainbenchmark.constants.RailwayQuery;
+import hu.bme.mit.trainbenchmark.railway.RailwayPackage;
+import hu.bme.mit.trainbenchmark.railway.Region;
+import hu.bme.mit.trainbenchmark.railway.Switch;
+import hu.bme.mit.trainbenchmark.railway.TrackElement;
+
+public class EmfApiQuerySwitchMonitored<TDriver extends EmfDriver> extends EmfApiQuery<EmfSwitchMonitoredMatch, TDriver> {
+
+	public EmfApiQuerySwitchMonitored(final TDriver driver) {
+		super(RailwayQuery.SWITCHMONITORED, driver);
+	}
+
+	@Override
+	public Collection<EmfSwitchMonitoredMatch> evaluate() {
+		final List<EmfSwitchMonitoredMatch> matches = new ArrayList<>();
+
+		final EList<Region> regions = driver.getContainer().getRegions();
+		for (final Region region : regions) {
+			for (final TrackElement element : region.getElements()) {
+				if (!RailwayPackage.eINSTANCE.getSwitch().isInstance(element)) {
+					continue;
+				}
+
+				// (sw:Switch)
+				final Switch sw = (Switch) element;
+
+				// (sw)-[:monitoredBy]->() NAC
+				if (sw.getMonitoredBy().isEmpty()) {
+					matches.add(new EmfSwitchMonitoredMatch(sw));
+				}
+			}
+		}
+
+		return matches;
+	}
+}
--- trainbenchmark-tool-epsilon/src/main/java/hu/bme/mit/trainbenchmark/benchmark/emfapi/operations/EpsilonModelOperationFactory.java
+++ trainbenchmark-tool-epsilon/src/main/java/hu/bme/mit/trainbenchmark/benchmark/emfapi/operations/EpsilonModelOperationFactory.java
@@ -44,7 +44,7 @@ import hu.bme.mit.trainbenchmark.benchmark.operations.ModelOperation;
 import hu.bme.mit.trainbenchmark.benchmark.operations.ModelOperationFactory;
 import hu.bme.mit.trainbenchmark.constants.RailwayOperation;
 
-public class EmfApiModelOperationFactory<TDriver extends EmfDriver> extends ModelOperationFactory<EmfMatch, TDriver> {
+public class EpsilonModelOperationFactory<TDriver extends EmfDriver> extends ModelOperationFactory<EmfMatch, TDriver> {
 
 	@Override
 	public ModelOperation<? extends EmfMatch, TDriver> createOperation(final RailwayOperation operationEnum, final String workspaceDir,
