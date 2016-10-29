adbfc46686e67e0830e8694fc5006a26df175093
Gabor Szarnyas <szarnyasg@gmail.com>
2016-10-28 21:31:58 -0400

    Minor changes & renames

--- trainbenchmark-tool-blazegraph/src/test/java/hu/bme/mit/trainbenchmark/benchmark/blazegraph/test/BlazegraphTest.java
+++ trainbenchmark-tool-blazegraph/src/test/java/hu/bme/mit/trainbenchmark/benchmark/blazegraph/test/BlazegraphTest.java
@@ -25,7 +25,7 @@ import hu.bme.mit.trainbenchmark.benchmark.runcomponents.BenchmarkResult;
 public class BlazegraphTest extends RdfTest {
 
 	@Override
-	protected BenchmarkResult runTest(BenchmarkConfigBase bcb) throws Exception {
+	protected BenchmarkResult runTest(final BenchmarkConfigBase bcb) throws Exception {
 		final BlazegraphBenchmarkConfig bc = new BlazegraphBenchmarkConfig(bcb, inferencing);
 		final BlazegraphBenchmarkScenario scenario = new BlazegraphBenchmarkScenario(bc);
 		final BenchmarkResult result = scenario.performBenchmark();
