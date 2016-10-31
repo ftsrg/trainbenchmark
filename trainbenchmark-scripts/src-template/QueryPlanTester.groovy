459596bc693cc03d9fd60a6366945ff8eb076154
Gabor Szarnyas <szarnyasg@gmail.com>
2016-10-31 13:29:51 -0400

    Fixed query plan tester script for iqdcore

--- trainbenchmark-scripts/src-template/QueryPlanTester.groovy
+++ trainbenchmark-scripts/src-template/QueryPlanTester.groovy
@@ -1,59 +1,45 @@
-//for (scenario in scenarios) {
-//	def scenarioString = scenario.toString().toLowerCase()
-//	def messageSize = 2048
-//
-//	def operations1 = [ACTIVEROUTE]
-//	for (variant in 'A'..'E') {
-//		for (size = minSize; size <= maxSize; size *= 2) {
-//			def modelPath = "railway-${scenarioString}-${size}"
-//			def bcRouteSensor = new BenchmarkConfigCore(xms, xmx, timeout, runs, queryTransformationCount, modelPath, operations1, "ActiveRoute")
-//			if (BenchmarkRunner.run(new IQDBenchmarkConfigWrapper(bcRouteSensor, messageSize, variant)) == 143) {
-//				break
-//			}
-//		}
-//	}
-//
-//	def operations2 = [ROUTESENSOR_REPAIR]
-//	for (variant in 'A'..'C') {
-//		for (size = minSize; size <= maxSize; size *= 2) {
-//			def modelPath = "railway-${scenarioString}-${size}"
-//			def bcRouteSensor = new BenchmarkConfigCore(xms, xmx, timeout, runs, queryTransformationCount, modelPath, operations2, "RouteSensor")
-//			if (BenchmarkRunner.run(new IQDBenchmarkConfigWrapper(bcRouteSensor, messageSize, variant)) == 143) {
-//				break
-//			}
-//		}
-//	}
-//
-//	def operations3 = [CONNECTEDSEGMENTS_REPAIR]
-//	for (variant in 'A'..'D') {
-//		for (size = minSize; size <= maxSize; size *= 2) {
-//			def modelPath = "railway-${scenarioString}-${size}"
-//			def bcConnectedSegments = new BenchmarkConfigCore(xms, xmx, timeout, runs, queryTransformationCount, modelPath, operations3, "ConnectedSegments")
-//			if (BenchmarkRunner.run(new IQDBenchmarkConfigWrapper(bcConnectedSegments, messageSize, variant)) == 143) {
-//				break
-//			}
-//		}
-//	}
-//
-//	def operations4 = [SEMAPHORENEIGHBOR_REPAIR]
-//	for (variant in 'A'..'F') {
-//		for (size = minSize; size <= maxSize; size *= 2) {
-//			def modelPath = "railway-${scenarioString}-${size}"
-//			def bcRouteSensor = new BenchmarkConfigCore(xms, xmx, timeout, runs, queryTransformationCount, modelPath, operations4, "SemaphoreNeighbor")
-//			if (BenchmarkRunner.run(new IQDBenchmarkConfigWrapper(bcRouteSensor, messageSize, variant)) == 143) {
-//				break
-//			}
-//		}
-//	}
-//
-//	def operations5 = [SWITCHSET_REPAIR]
-//	for (variant in 'A'..'B') {
-//		for (size = minSize; size <= maxSize; size *= 2) {
-//			def modelPath = "railway-${scenarioString}-${size}"
-//			def bcRouteSensor = new BenchmarkConfigCore(xms, xmx, timeout, runs, queryTransformationCount, modelPath, operations5, "SwitchSet")
-//			if (BenchmarkRunner.run(new IQDBenchmarkConfigWrapper(bcRouteSensor, messageSize, variant)) == 143) {
-//				break
-//			}
-//		}
-//	}
-//}
+import hu.bme.mit.trainbenchmark.benchmark.config.BenchmarkConfigBase
+import hu.bme.mit.trainbenchmark.benchmark.iqdcore.config.IqdCoreBenchmarkConfig
+import hu.bme.mit.trainbenchmark.benchmark.runcomponents.BenchmarkRunner
+import hu.bme.mit.trainbenchmark.constants.RailwayOperation
+import hu.bme.mit.trainbenchmark.constants.Scenario
+
+def scenarios = [Scenario.REPAIR]
+def minSize = 1
+def maxSize = 2
+def xms = "2G"
+def xmx = "2G"
+def timeout = 60
+def runs = 5
+def queryTransformationCount = 10
+
+for (scenario in scenarios) {
+	def scenarioString = scenario.toString().toLowerCase()
+	def messageSize = 2048
+
+	def operationsRouteSensor = [
+		RailwayOperation.ROUTESENSOR_REPAIR
+	]
+	for (variant in 'A'..'C') {
+		for (size = minSize; size <= maxSize; size *= 2) {
+			def modelPath = "railway-${scenarioString}-${size}"
+			def bc = new BenchmarkConfigBase(xms, xmx, timeout, runs, queryTransformationCount, modelPath, operationsRouteSensor, "RouteSensor")
+			if (BenchmarkRunner.run(new IqdCoreBenchmarkConfig(bc, messageSize, variant, null)) == 143) {
+				break
+			}
+		}
+	}
+
+	def operationsSemaphoreNeighbor = [
+		RailwayOperation.SEMAPHORENEIGHBOR_REPAIR
+	]
+	for (variant in 'A'..'F') {
+		for (size = minSize; size <= maxSize; size *= 2) {
+			def modelPath = "railway-${scenarioString}-${size}"
+			def bc = new BenchmarkConfigBase(xms, xmx, timeout, runs, queryTransformationCount, modelPath, operationsSemaphoreNeighbor, "SemaphoreNeighbor")
+			if (BenchmarkRunner.run(new IqdCoreBenchmarkConfig(bc, messageSize, variant, null)) == 143) {
+				break
+			}
+		}
+	}
+}
