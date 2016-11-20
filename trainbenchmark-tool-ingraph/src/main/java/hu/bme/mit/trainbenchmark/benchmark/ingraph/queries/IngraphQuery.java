51ac709162cc35597f049e616f8be2c208e72838
János Maginecz <wafleeee@gmail.com>
2016-11-20 19:02:54 +0100

    Implement changelistener in Ingraph

--- trainbenchmark-tool-ingraph/src/main/java/hu/bme/mit/trainbenchmark/benchmark/ingraph/queries/IngraphQuery.java
+++ trainbenchmark-tool-ingraph/src/main/java/hu/bme/mit/trainbenchmark/benchmark/ingraph/queries/IngraphQuery.java
@@ -1,4 +1,4 @@
-/*******************************************************************************
+	/*******************************************************************************
  * Copyright (c) 2010-2015, Benedek Izso, Gabor Szarnyas, Istvan Rath and Daniel Varro
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -31,7 +31,7 @@ import scala.collection.immutable.Map;
 public class IngraphQuery<TPatternMatch extends IngraphMatch> extends ModelQuery<TPatternMatch, IngraphDriver> {
 
 	private TrainbenchmarkQuery queryEngine;
-	
+	private IngraphChangeListener listener;
 	public IngraphQuery(final IngraphDriver driver, final RailwayQuery query, final TransactionFactory input)
 			throws IOException {
 		super(query, driver);
@@ -39,6 +39,8 @@ public class IngraphQuery<TPatternMatch extends IngraphMatch> extends ModelQuery
 			final RelalgContainer plan = IngraphUtils.getQueryPlan(query.toString(), driver.getQueryVariant());
 			final IngraphAdapter adapter = new IngraphAdapter(plan);
 			queryEngine = adapter.engine();
+			listener = new IngraphChangeListener(query);
+			queryEngine.addListener(listener);
 			input.subscribe(queryEngine.inputLookup());
 			driver.setAdapter(adapter);
 		} catch (final Exception e) {
@@ -53,14 +55,8 @@ public class IngraphQuery<TPatternMatch extends IngraphMatch> extends ModelQuery
 		driver.maybeMeasureMemory();
 
 		final List<IngraphMatch> matches = new ArrayList<>();
-
-		final Iterator<Map<Object, Object>> resultIterator = queryEngine.getResults().iterator();
-		while (resultIterator.hasNext()) {
-			final Map<Object, Object> qs = resultIterator.next();
-			final IngraphMatch match = IngraphMatch.createMatch(query, qs);
-			matches.add(match);
-		}
-		return (Collection<TPatternMatch>) matches;
+		queryEngine.getResults();
+		return (Collection<TPatternMatch>) listener.results;
 	}
 
 }
