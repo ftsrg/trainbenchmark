f0b02e2709d6d44d2f1ec807acb20e569f6769e4
Gabor Szarnyas <szarnyasg@gmail.com>
2016-11-22 00:18:18 -0500

    R script axis size

--- trainbenchmark-reporting/report.R
+++ trainbenchmark-reporting/report.R
@@ -129,7 +129,7 @@ for (workload in workloads) {
       legend.key = element_blank(), 
       legend.title = element_blank(), 
       legend.position = "bottom",
-      axis.text = element_text(size = 7)
+      axis.text = element_text(size = 5.5)
     )
   print(p)
   
