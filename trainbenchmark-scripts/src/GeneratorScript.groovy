0dd3c0001c1aa8c856a1f54f35d81eb8292f549e
Gabor Szarnyas <szarnyasg@gmail.com>
2017-10-05 10:51:47 -0400

    Add GeneratorScript for reproducibility

---
+++ trainbenchmark-scripts/src/GeneratorScript.groovy
@@ -0,0 +1,45 @@
+import hu.bme.mit.trainbenchmark.config.ExecutionConfig
+import hu.bme.mit.trainbenchmark.generator.config.GeneratorConfigBase
+import hu.bme.mit.trainbenchmark.generator.config.Scenario
+import hu.bme.mit.trainbenchmark.generator.emf.config.EmfGeneratorConfigBuilder
+import hu.bme.mit.trainbenchmark.generator.graph.neo4j.config.Neo4jGraphGeneratorConfigBuilder
+import hu.bme.mit.trainbenchmark.generator.graph.tinkerpop.config.TinkerGraphFormat
+import hu.bme.mit.trainbenchmark.generator.graph.tinkerpop.config.TinkerGraphGeneratorConfigBuilder
+import hu.bme.mit.trainbenchmark.generator.rdf.config.RdfGeneratorConfigBuilder
+import hu.bme.mit.trainbenchmark.generator.runner.GeneratorRunner
+import hu.bme.mit.trainbenchmark.generator.sql.config.SqlGeneratorConfigBuilder
+import hu.bme.mit.trainbenchmark.neo4j.config.Neo4jGraphFormat;
+import hu.bme.mit.trainbenchmark.rdf.RdfFormat
+
+def ec = new ExecutionConfig(4000, 6000)
+def minSize = 1
+def maxSize = 512
+
+def scenarios = [
+	Scenario.INJECT,
+]
+
+def formats = [
+	new EmfGeneratorConfigBuilder(),
+]
+
+for (scenario in scenarios) {
+	formats.each { generatorConfigBuilder ->
+		try {
+			for (def size = minSize; size <= maxSize; size *= 2) {
+				println("Scenario: ${scenario}, size: ${size}")
+
+				def configBase = new GeneratorConfigBase(scenario, size)
+				def config = generatorConfigBuilder.setConfigBase(configBase).createConfig()
+
+				def exitValue = GeneratorRunner.run(config, ec)
+				if (exitValue != 0) {
+					println "Timeout or error occured, skipping models for larger sizes. Error code: ${exitValue}"
+					break
+				}
+			}
+		} catch (all) {
+			println "Exception occured during execution."
+		}
+	}
+}
