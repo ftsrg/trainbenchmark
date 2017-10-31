adf4d3ff9f06e69689d1f0b48b0253d9806bfc04
Gabor Szarnyas <szarnyasg@gmail.com>
2017-10-31 15:34:49 -0400

    Make get-neo4j.sh script idempotent

--- scripts/get-neo4j.sh
+++ scripts/get-neo4j.sh
@@ -3,6 +3,7 @@
 NEO4J_VERSION=3.2.5
 
 cd "$( cd "$( dirname "$0" )" && pwd )/../"
+rm -rf neo4j-server
 wget https://neo4j.com/artifact.php?name=neo4j-community-$NEO4J_VERSION-unix.tar.gz -O neo4j.tar.gz
 tar xf neo4j.tar.gz
 mv neo4j-community-$NEO4J_VERSION neo4j-server
