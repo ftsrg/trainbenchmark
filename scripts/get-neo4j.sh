ab677546de2330cc700618d43be4ae16c1e59be2
Gabor Szarnyas <szarnyasg@gmail.com>
2017-11-27 18:21:36 -0500

    Update wget script, list dirs

--- scripts/get-neo4j.sh
+++ scripts/get-neo4j.sh
@@ -4,6 +4,7 @@ NEO4J_VERSION=3.3.0
 
 cd "$( cd "$( dirname "$0" )" && pwd )/../"
 rm -rf neo4j-server
-wget https://neo4j.com/artifact.php?name=neo4j-community-$NEO4J_VERSION-unix.tar.gz -O neo4j.tar.gz
+wget https://neo4j.com/artifact.php?name=neo4j-community-$NEO4J_VERSION-unix.tar.gz -O neo4j.tar.gz --progress=bar
 tar xf neo4j.tar.gz
 mv neo4j-community-$NEO4J_VERSION neo4j-server
+ls neo4j-server
