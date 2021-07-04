e9e1220877277cde17116bdeb649fb539ce9c602
Gabor Szarnyas <szarnyasg@gmail.com>
2021-07-04 23:19:31 +0200

    Use quiet mode in wget

--- scripts/get-neo4j.sh
+++ scripts/get-neo4j.sh
@@ -4,6 +4,6 @@ NEO4J_VERSION=3.3.0
 
 cd "$( cd "$( dirname "$0" )" && pwd )/../"
 rm -rf neo4j-server
-wget https://neo4j.com/artifact.php?name=neo4j-community-$NEO4J_VERSION-unix.tar.gz -O neo4j.tar.gz
+wget -q https://neo4j.com/artifact.php?name=neo4j-community-$NEO4J_VERSION-unix.tar.gz -O neo4j.tar.gz
 tar xf neo4j.tar.gz
 mv neo4j-community-$NEO4J_VERSION neo4j-server
